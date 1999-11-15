f61e9f4fc3cd8759589d52dbd75c856e59308ed6
scott <>
1999-11-15 20:20:37 +0000

    Added include for config.h.  Minor code cleanup.  Removed assignments from if checks; first makes assignment, then checks for appropriate value.  Made private member variable names more descriptive.  OpenLinkedFile now checks to see if the file is writable or not, and still opens the file, even if it can't be written to.  This needs to be more robust (i.e., we shouldn't be creating temp files if the file isn't writable).  Made use of bFileWrtiable variable, which was introduced.

--- src/id3/tag_file.cpp
+++ src/id3/tag_file.cpp
@@ -14,43 +14,54 @@
 //
 //  Mon Nov 23 18:34:01 1998
 
+#if defined HAVE_CONFIG_H
+#include <config.h>
+#endif
 
 #include <string.h>
 #include <id3/tag.h>
 
-
 bool exists(char *name)
 {
   bool doesExist = false;
-  FILE *in;
+  FILE *in = NULL;
   
-  if (name)
-  {
-    if (in = fopen(name, "rb"))
-    {
-      doesExist = true;
-      fclose(in);
-    }
-  }
-  else
+  if (NULL == name)
     ID3_THROW(ID3E_NoData);
+
+  in = fopen(name, "rb");
+  doesExist = (NULL != in);
+  if (doesExist)
+    fclose(in);
     
   return doesExist;
 }
 
-
 void ID3_Tag::OpenLinkedFile(void)
 {
-  char *mode = "rb+";
-  
-  if (! exists(fileName))
-    mode = "wb+";
-    
-  if (fileHandle = fopen(fileName, mode))
+  if (!exists(__sFileName))
+    // Create a new file if the filename doesn't exist
+    __fFileHandle = fopen(__sFileName, "wb+");
+  else
   {
-    fseek(fileHandle, 0, SEEK_END);
-    fileSize = ftell(fileHandle);
-    fseek(fileHandle, 0, SEEK_SET);
+    // Try to open the file for reading and writing. If that doesn't work, open
+    // it for just reading.
+    __fFileHandle = fopen(__sFileName, "rb+");
+    __bFileWritable = (__fFileHandle != NULL);
+    if (!__bFileWritable)
+    {
+      __fFileHandle = fopen(__sFileName, "rb");
+    }
+  }
+  
+  if (NULL == __fFileHandle)
+    // File could not be opened, either for reading or writing
+    ID3_THROW(ID3E_NoFile);
+  else
+  {
+    fseek(__fFileHandle, 0, SEEK_END);
+    __ulFileSize = ftell(__fFileHandle);
+    fseek(__fFileHandle, 0, SEEK_SET);
   }
   
   return ;
@@ -61,27 +72,25 @@ luint ID3_Tag::Link(char *fileInfo)
 {
   luint posn = 0;
   
-  if (fileInfo)
-  {
-    strncpy(fileName, fileInfo, 256);
-    
-    // if we were attached to some other file
-    // file then abort
-    if (fileHandle)
-      ID3_THROW(ID3E_TagAlreadyAttached);
-      
-    GenerateTempName();
-    OpenLinkedFile();
-    oldTagSize = ParseFromHandle();
-    
-    if (oldTagSize)
-      oldTagSize += ID3_TAGHEADERSIZE;
-      
-    fileSize -= oldTagSize;
-    posn = oldTagSize;
-  }
-  else
+  if (NULL == fileInfo)
     ID3_THROW(ID3E_NoData);
+
+  strncpy(__sFileName, fileInfo, 256);
+    
+  // if we were attached to some other file then abort
+  if (__fFileHandle != NULL)
+    ID3_THROW(ID3E_TagAlreadyAttached);
+      
+  OpenLinkedFile();
+  if (__bFileWritable)
+    GenerateTempName();
+  __ulOldTagSize = ParseFromHandle();
+    
+  if (__ulOldTagSize > 0)
+    __ulOldTagSize += ID3_TAGHEADERSIZE;
+      
+  __ulFileSize -= __ulOldTagSize;
+  posn = __ulOldTagSize;
     
   return posn;
 }
@@ -89,6 +98,9 @@ luint ID3_Tag::Link(char *fileInfo)
 
 void ID3_Tag::Update(void)
 {
+  if (!__bFileWritable)
+    ID3_THROW_DESC(ID3E_NoFile, "ID3_Tag::Update: File not writable.");
+
   if (HasChanged())
     RenderToHandle();
     
@@ -100,56 +112,61 @@ void ID3_Tag::Strip(bool v1Also)
 {
   FILE *tempOut;
   
-  if (strlen(tempName) > 0)
+  if (!__bFileWritable)
+    ID3_THROW_DESC(ID3E_NoFile, "ID3_Tag::Strip: File not writable.");
+
+  if (strlen(__sTempName) > 0)
   {
-    if (tempOut = fopen(tempName, "wb"))
+    tempOut = fopen(__sTempName, "wb");
+    if (tempOut != NULL)
     {
       uchar * buffer2;
       
-      fseek(fileHandle, oldTagSize, SEEK_SET);
+      fseek(__fFileHandle, __ulOldTagSize, SEEK_SET);
       
-      if (buffer2 = new uchar[BUFF_SIZE])
+      buffer2 = new uchar[BUFF_SIZE];
+      if (NULL == buffer2)
+        ID3_THROW(ID3E_NoMemory);
+
+      bool done = false;
+      luint bytesCopied = 0;
+      luint bytesToCopy = __ulFileSize;
+      size_t i;
+        
+      if (__bHasV1Tag && v1Also)
+        bytesToCopy -= __ulExtraBytes;
+          
+      while (! feof(__fFileHandle) && ! done)
       {
-        bool done = false;
-        luint bytesCopied = 0;
-        luint bytesToCopy = fileSize;
-        int i;
-        
-        if (hasV1Tag && v1Also)
-          bytesToCopy -= extraBytes;
+        luint size = BUFF_SIZE;
           
-        while (! feof(fileHandle) && ! done)
+        if ((bytesToCopy - bytesCopied) < BUFF_SIZE)
         {
-          luint size = BUFF_SIZE;
-          
-          if ((bytesToCopy - bytesCopied) < BUFF_SIZE)
-          {
-            size = bytesToCopy - bytesCopied;
-            done = true;
-          }
-          
-          if (i = fread(buffer2, 1, size, fileHandle))
-            fwrite(buffer2, 1, i, tempOut);
-            
-          bytesCopied += i;
+          size = bytesToCopy - bytesCopied;
+          done = true;
         }
-        
-        delete[] buffer2;
+          
+        i = fread(buffer2, 1, size, __fFileHandle);
+        if (i > 0)
+          fwrite(buffer2, 1, i, tempOut);
+            
+        bytesCopied += i;
       }
+        
+      delete[] buffer2;
       
       fclose(tempOut);
-      fclose(fileHandle);
-      remove(fileName);
-      rename(tempName, fileName);
+      fclose(__fFileHandle);
+      remove(__sFileName);
+      rename(__sTempName, __sFileName);
       OpenLinkedFile();
       
-      oldTagSize = 0;
-      extraBytes = 0;
+      __ulOldTagSize = 0;
+      __ulExtraBytes = 0;
       
-      if (v1Also)
-        hasV1Tag = false;
+      __bHasV1Tag = __bHasV1Tag && !v1Also;
         
-      hasChanged = true;
+      __bHasChanged = true;
     }
   }
   
@@ -159,3 +176,6 @@ void ID3_Tag::Strip(bool v1Also)
 
 
 // $Log$
+// Revision 1.3  1999/11/04 04:15:55  scott
+// Added cvs Id and Log tags to beginning and end of file, respectively.
+//
