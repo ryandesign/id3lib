9bada13d90e19fed77988a2f26adbaeb43aae550
scott <>
1999-11-19 18:59:09 +0000

    * misc_support.h: Added the function declarations for ID3_GetString, ID3_GetArtist, ID3_GetAlbum, ID3_GetTitle, ID3_GetYear, ID3_AddYear, ID3_GetComment, ID3_AddComment, ID3_GetTrack, ID3_GetTrackNum, ID3_AddTrack, ID3_GetGenre, ID3_GetGenreNum, and ID3_GetLyrics.

--- src/id3/misc_support.h
+++ src/id3/misc_support.h
@@ -23,15 +23,33 @@
 #include <id3/tag.h>
 
 // in 'id3_misc_support.cpp'
-void ID3_AddTitle(ID3_Tag *tag, char *text);
-void ID3_AddArtist(ID3_Tag *tag, char *text);
-void ID3_AddAlbum(ID3_Tag *tag, char *text);
-void ID3_AddLyrics(ID3_Tag *tag, char *text);
-void ID3_ASCIItoUnicode(wchar_t *unicode, char *ascii, luint len);
-void ID3_UnicodeToASCII(char *ascii, wchar_t *unicode, luint len);
+char *ID3_GetArtist(ID3_Tag *tag);
+bool  ID3_AddArtist(ID3_Tag *tag, char *text);
+char *ID3_GetAlbum(ID3_Tag *tag);
+bool  ID3_AddAlbum(ID3_Tag *tag, char *text);
+char *ID3_GetTitle(ID3_Tag *tag);
+bool  ID3_AddTitle(ID3_Tag *tag, char *text);
+char *ID3_GetYear(ID3_Tag *tag);
+bool  ID3_AddYear(ID3_Tag *tag, char *text);
+char *ID3_GetComment(ID3_Tag *tag);
+bool  ID3_AddComment(ID3_Tag *tag, char *text);
+char *ID3_GetTrack(ID3_Tag *tag);
+luint ID3_GetTrackNum(ID3_Tag *tag);
+bool  ID3_AddTrack(ID3_Tag *tag, uchar ucTrack, uchar ucTotal = 0);
+char *ID3_GetGenre(ID3_Tag *tag);
+luint ID3_GetGenreNum(ID3_Tag *tag);
+bool  ID3_AddGenre(ID3_Tag *tag, luint ucGenre);
+char *ID3_GetLyrics(ID3_Tag *tag);
+bool  ID3_AddLyrics(ID3_Tag *tag, char *text);
+void  ID3_ASCIItoUnicode(wchar_t *unicode, const char *ascii, const luint len);
+void  ID3_UnicodeToASCII(char *ascii, const wchar_t *unicode, const luint len);
+char *ID3_GetString(const ID3_Frame *frame, const ID3_FieldID fldName);
 // in 'id3_tag_parse_v1.cpp'
 void ID3_RemoveTrailingSpaces(char *buffer, luint length);
 
 #endif
 
 // $Log$
+// Revision 1.4  1999/11/04 04:15:55  scott
+// Added cvs Id and Log tags to beginning and end of file, respectively.
+//
