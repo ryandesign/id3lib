a1c7e759b7e94eb3dcfd0df3bda1dc1750319f74
scott <>
1999-11-25 18:51:34 +0000

    (Render): Replaced manual array initialization with call to memset. (GenerateTempName): Removed method.  No longer used. (RenderV1): New method.  Renders an id3v1 tag to an array. (RenderV1ToHandle): Moved work of writing tag to array to RenderV1. (RenderV2ToHandle): Rather than generating a temporary file name with mktemp (or similar) and opening that file name to a temporary file, we now use tmpfile to create a temporary file directly.  Unfortunately, this means we can't just rename the tempfile to the original file when all the copying is done, so we have to perform a second copy from the temp file back to the original.  This makes the code less efficient but more portable, as we rely on tmpfile for knowing where to create the temporary file.  To improve, we'll need to figure out a portable mechanism for creating temporary files (i.e., resolve the differences between /tmp/ and C:\temp\).

--- src/id3/tag_render.cpp
+++ src/id3/tag_render.cpp
@@ -59,10 +59,9 @@ luint ID3_Tag::Render(uchar *buffer)
   {
     uchar *tempz;
     luint newTagSize;
-      
+    
     newTagSize = GetUnSyncSize(&buffer[header.Size()], 
                                bytesUsed - header.Size());
-      
     if (newTagSize > 0 && (newTagSize + header.Size()) > bytesUsed)
     {
       tempz = new uchar[newTagSize];
@@ -72,7 +71,7 @@ luint ID3_Tag::Render(uchar *buffer)
       UnSync(tempz, newTagSize, &buffer[header.Size()],
              bytesUsed - header.Size());
       header.SetFlags(ID3HF_UNSYNC);
-          
+
       memcpy(&buffer[header.Size()], tempz, newTagSize);
       bytesUsed = newTagSize + header.Size();
       delete[] tempz;
@@ -80,10 +79,10 @@ luint ID3_Tag::Render(uchar *buffer)
   }
     
   // zero the remainder of the buffer so that our padding bytes are zero
-  for (luint i = 0; i < PaddingSize(bytesUsed); i++)
-    buffer[bytesUsed + i] = 0;
-      
-  bytesUsed += PaddingSize(bytesUsed);
+  luint nPadding = PaddingSize(bytesUsed);
+
+  memset(&buffer[bytesUsed], '\0', nPadding);
+  bytesUsed += nPadding;
     
   header.SetDataSize(bytesUsed - header.Size());
   header.Render(buffer);
@@ -93,7 +92,6 @@ luint ID3_Tag::Render(uchar *buffer)
   
   return bytesUsed;
 }
-
 
 luint ID3_Tag::Size(void) const
 {
@@ -134,30 +132,29 @@ void ID3_Tag::RenderExtHeader(uchar *buffer)
 }
 
 
-void ID3_Tag::GenerateTempName(void)
+luint ID3_Tag::RenderV1(char *buffer)
 {
-  strcpy(__sTempName, __sFileName);
-  strcat(__sTempName, "XXXXXX");
-  if (mktemp(__sTempName) == NULL)
-  {
-    // Couldn't create a unique temporary name
-    // Probably should throw an exception to that effect
-  }
-  
-  return ;
-}
+  // Sanity check our buffer
+  if (NULL == buffer)
+    ID3_THROW(ID3E_NoBuffer);
 
-void ID3_Tag::RenderV1ToHandle(void)
-{
-  ID3V1_Tag tagV1;
-  char sTag[LEN_V1], *pCur = sTag, *sTemp = NULL;
+  // pCur is used to mark where to next write in the buffer
+  // sTemp is used as a temporary string pointer for functions that return
+  //  dynamically created strings
+  char *pCur = buffer, *sTemp = NULL;
 
-  for (int i = 0; i < LEN_V1; i++)
-    sTag[i] = '\0';
+  // The default char for a v1 tag is null
+  // Note: Using strncpy should make this unnecessary, as strncpy will pad
+  //  the destination string with nulls.  But unless it becomes a performance
+  //  issue (I doubt it, for 128 bit strings), its an extra layer of
+  //  protection.
+  memset(buffer, '\0', LEN_V1);
 
+  // Write the TAG identifier
   strncpy(pCur, "TAG", LEN_V1_ID);
   pCur = &pCur[LEN_V1_ID];
 
+  // Write the TITLE
   sTemp = ID3_GetTitle(this);
   if (sTemp != NULL)
   {
@@ -166,6 +163,7 @@ void ID3_Tag::RenderV1ToHandle(void)
   }
   pCur = &pCur[LEN_V1_TITLE];
 
+  // Write the ARTIST
   sTemp = ID3_GetArtist(this);
   if (sTemp != NULL)
   {
@@ -174,6 +172,7 @@ void ID3_Tag::RenderV1ToHandle(void)
   }
   pCur = &pCur[LEN_V1_ARTIST];
 
+  // Write the ALBUM
   sTemp = ID3_GetAlbum(this);
   if (sTemp != NULL)
   {
@@ -182,6 +181,7 @@ void ID3_Tag::RenderV1ToHandle(void)
   }
   pCur = &pCur[LEN_V1_ALBUM];
 
+  // Write the YEAR
   sTemp = ID3_GetYear(this);
   if (sTemp != NULL)
   {
@@ -190,24 +190,38 @@ void ID3_Tag::RenderV1ToHandle(void)
   }
   pCur = &pCur[LEN_V1_YEAR];
 
+  // Write the COMMENT
   sTemp = ID3_GetComment(this);
   if (sTemp != NULL)
   {
     strncpy(pCur, sTemp, LEN_V1_COMMENT);
     delete [] sTemp;
   }
-  pCur = &pCur[LEN_V1 - 2];
+  pCur = &pCur[LEN_V1_COMMENT];
 
-  sTemp = ID3_GetTrack(this);
-  if (sTemp != NULL)
+  // Write the TRACK, if it isn't 0
+  luint nTrack = ID3_GetTrackNum(this);
+  if (0 != nTrack)
   {
-    uchar ucTrack = (uchar) atoi(sTemp);
+    pCur -= 2;
     pCur[0] = '\0';
-    pCur[1] = ucTrack;
-    delete [] sTemp;
+    pCur[1] = (uchar) nTrack;
+    pCur += 2;
   }
 
+  // Write the GENRE
+  pCur[0] = (uchar) ID3_GetGenreNum(this);
+
+  return LEN_V1;
+}
+
+void ID3_Tag::RenderV1ToHandle(void)
+{
+  ID3V1_Tag tagV1;
+  char sTag[LEN_V1];
   char sID[LEN_V1_ID];
+
+  RenderV1(sTag);
 
   // We want to check if there is already an id3v1 tag, so we can write over
   // it.  First, seek to the beginning of any possible id3v1 tag
@@ -223,15 +237,19 @@ void ID3_Tag::RenderV1ToHandle(void)
   // If those three characters are TAG, then there's a preexisting id3v1 tag,
   // so we should set the file cursor so we can overwrite it with a new tag.
   if (memcmp(sID, "TAG", LEN_V1_ID) == 0)
+  {
     if (fseek(__fFileHandle, -LEN_V1, SEEK_END) != 0)
       // TODO:  This is a bad error message.  Make it more descriptive
       ID3_THROW(ID3E_NoData);
+  }
   // Otherwise, set the cursor to the end of the file so we can append on 
   // the new tag.
   else
+  {
     if (fseek(__fFileHandle, 0, SEEK_END) != 0)
       // TODO:  This is a bad error message.  Make it more descriptive
       ID3_THROW(ID3E_NoData);
+  }
 
   fwrite(sTag, 1, LEN_V1, __fFileHandle);
   __bHasV1Tag = true;
@@ -245,70 +263,65 @@ void ID3_Tag::RenderV2ToHandle(void)
   if (NULL == __fFileHandle)
     ID3_THROW(ID3E_NoData);
 
-  if (size > 0)
+  if (size == 0)
+    return;
+
+  buffer = new uchar[size];
+  if (NULL == buffer)
+    ID3_THROW(ID3E_NoMemory);
+
+  size = Render(buffer);      
+  if (0 == size)
   {
-    buffer = new uchar[size];
-    if (NULL == buffer)
-      ID3_THROW(ID3E_NoMemory);
-
-    size = Render(buffer);      
-    if (size > 0)
-    {
-      // if the new tag fits perfectly within the old and the old one
-      // actually existed (ie this isn't the first tag this file has had)
-      if ((__ulOldTagSize == 0 && __ulFileSize == 0) || 
-          (size == __ulOldTagSize && size != 0))
-      {
-        fseek(__fFileHandle, 0, SEEK_SET);
-        fwrite(buffer, 1, size, __fFileHandle);
-        __ulOldTagSize = size;
-      }
-      else
-      {
-        // else we gotta make a temp file, copy the tag into it, copy the
-        // rest of the old file after the tag, delete the old file, rename
-        // this new file to the old file's name and update the __fFileHandle
-        FILE *tempOut;
-            
-        if (strlen(__sTempName) > 0)
-        {
-          tempOut = fopen(__sTempName, "wb");
-          if (NULL != tempOut)
-          {
-            uchar * buffer2;
-                
-            fwrite(buffer, 1, size, tempOut);
-                
-            fseek(__fFileHandle, __ulOldTagSize, SEEK_SET);
-                
-            buffer2 = new uchar [BUFF_SIZE];
-            if (NULL == buffer2)
-              ID3_THROW(ID3E_NoMemory);
-
-            int i;
-                  
-            while (! feof(__fFileHandle))
-            {
-              i = fread(buffer2, 1, BUFF_SIZE, __fFileHandle);
-              fwrite(buffer2, 1, i, tempOut);
-            }
-              
-            delete[] buffer2;
-                
-            fclose(tempOut);
-            fclose(__fFileHandle);
-            remove(__sFileName);
-            rename(__sTempName, __sFileName);
-            OpenLinkedFile();
-                
-            __ulOldTagSize = size;
-          }
-        }
-      }
-    }
-        
-    delete[] buffer;
+    delete [] buffer;
+    return;
   }
+
+  // if the new tag fits perfectly within the old and the old one
+  // actually existed (ie this isn't the first tag this file has had)
+  if ((0 == __ulOldTagSize && 0 == __ulFileSize) || 
+      (size == __ulOldTagSize && size != 0))
+  {
+    fseek(__fFileHandle, 0, SEEK_SET);
+    fwrite(buffer, 1, size, __fFileHandle);
+    __ulOldTagSize = size;
+  }
+  else
+  {
+    // else we gotta make a temp file, copy the tag into it, copy the
+    // rest of the old file after the tag, delete the old file, rename
+    // this new file to the old file's name and update the __fFileHandle
+            
+    uchar buffer2[BUFF_SIZE];
+    FILE *tempOut = tmpfile();
+    if (NULL == tempOut)
+      ID3_THROW(ID3E_ReadOnly);
+
+    fwrite(buffer, 1, size, tempOut);
+                
+    fseek(__fFileHandle, __ulOldTagSize, SEEK_SET);
+      
+    while (! feof(__fFileHandle))
+    {
+      size_t nBytes = fread(buffer2, 1, BUFF_SIZE, __fFileHandle);
+      fwrite(buffer2, 1, nBytes, tempOut);
+    }
+      
+    rewind(tempOut);
+    freopen(__sFileName, "w+", __fFileHandle);
+
+    while (!feof(tempOut))
+    {
+      size_t nBytes = fread(buffer2, 1, BUFF_SIZE, tempOut);
+      fwrite(buffer2, 1, nBytes, __fFileHandle);
+    }
+
+    fclose(tempOut);
+    
+    __ulOldTagSize = size;
+  }
+        
+  delete[] buffer;
     
   return ;
 }
@@ -331,7 +344,9 @@ luint ID3_Tag::PaddingSize(luint curSize) const
   // shuffling the rest of the song file around
   if (__ulOldTagSize && (__ulOldTagSize > curSize) && 
       (curSize - __ulOldTagSize) < ID3_PADMAX)
+  {
     newSize = __ulOldTagSize;
+  }
   else
   {
     luint tempSize = curSize + __ulFileSize;
@@ -351,6 +366,11 @@ luint ID3_Tag::PaddingSize(luint curSize) const
 
 
 // $Log$
+// Revision 1.6  1999/11/19 19:27:27  scott
+// (RenderV1ToHandle): Added.  Adds or overwrites an id3v1 tag to a file
+// handle.  Needs testing.
+// (RenderV2ToHandle): Renamed from RenderToHandle.
+//
 // Revision 1.5  1999/11/15 20:21:29  scott
 // Added include for config.h.  Minor code cleanup.  Removed
 // assignments from if checks; first makes assignment, then checks
