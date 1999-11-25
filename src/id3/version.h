c0c2de3c007a727caefbafa73ba04efa2a117a32
scott <>
1999-11-25 19:25:46 +0000

    Added doc++/kdoc/javadoc-like documentation.  Replaced #define's with const's.

--- src/id3/version.h
+++ src/id3/version.h
@@ -16,14 +16,25 @@
 
 #ifndef ID3LIB_VERSION_H
 #define ID3LIB_VERSION_H
-
-#define ID3LIB_NAME             "ID3Lib"
-#define ID3LIB_VERSION          "v3.05a"
-#define ID3LIB_VER              (3)
-#define ID3LIB_REV              (5)
-#define ID3LIB_DATE             "Mon Nov 23 18:34:01 1998"
-#define ID3LIB_VERSTRING        ID3LIB_NAME " " ID3LIB_VERSION " " ID3LIB_DATE
+/**
+ * id3lib defines some constant symbols that applications are free to use.
+ * They are used to identify the version and date of the id3lib that is
+ * currently being used.
+ *
+ * For example...
+ *
+ * <pre>cout << "Using " << ID3LIB_VERSTRING << endl;</pre>
+ */
+const char * const ID3LIB_NAME      = "ID3Lib";
+const char * const ID3LIB_VERSION   = "v3.6.0";
+const luint        ID3LIB_VER       = 3;
+const luint        ID3LIB_REV       = 6;
+const char * const ID3LIB_DATE      = "Sat Nov 20 11:27:25 CST 1999";
+#define     ID3LIB_VERSTRING   ID3LIB_NAME " " ID3LIB_VERSION " " ID3LIB_DATE
 
 #endif
 
 // $Log$
+// Revision 1.4  1999/11/04 04:15:55  scott
+// Added cvs Id and Log tags to beginning and end of file, respectively.
+//
