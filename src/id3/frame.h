74d38f3d92dbac47f9be1c4846dc6ec31a88d324
scott <>
1999-11-25 19:15:15 +0000

    Added doc++/kdoc/javadoc-like documentation, mostly transcribed from Dirk's ID3Lib Documentation MSWord doc.

--- src/id3/frame.h
+++ src/id3/frame.h
@@ -20,16 +20,89 @@
 
 class ID3_Tag;
 
+/**
+ * id3lib defines frames in a funny way.  Using some nice c++ conventions,
+ * ID3_Frame class objects appear to be quite polymorphic; that is, they can
+ * take on many forms.  The same ID3_Frame class provides the facilities for
+ * the implementation of a complex APIC frame and for a simple text frame.
+ * Here's how to use an ID3_Frame object...
+ *
+ * @short The representative class of an id3v2 frame.
+ * @author Dirk Mahoney
+ * @version $Id$
+ * @see ID3_Tag
+ * @see ID3_Field
+ * @see ID3_Err
+ */
 class ID3_Frame
 {
   friend ID3_Tag;
 public:
+  /** 
+   * The optional parameter to the ID3_Frame constructor is of the type
+   * @ref ID3_FrameID, which will internally set the frame's structure.  See
+   * @ref #SetID() for more details.
+   *
+   * @param id The type of frame to create
+   * @see ID3_FrameID
+   */
   ID3_Frame(ID3_FrameID id = ID3FID_NOFRAME);
+
+  /**
+   */
   ~ID3_Frame(void);
   
+  /** 
+   * Like its @ref ID3_Tag counterpart, @ref #Clear takes no parameters and
+   * returns nothing.  It just clears the frame of all data and resets the
+   * frame such that it can take on the form of any id3v2 frame that id3lib
+   * supports.
+   *
+   * @see ID3_Tag#Clear
+   */
   void        Clear(void);
+
+  /**
+   * This method sets up the internal structure of an ID3_Frame object so
+   * that it represents any id3v2 frame that id3lib suppors.  Given an
+   * @ref ID3_FrameID (a list of which is found in <id3/field.h>), @ref #SetID
+   * will structure the object according to the frame you wish to implement.
+   *
+   * Either using this call or via the constructor, this must be the first
+   * command performed on an ID3_Frame object.  
+   *
+   * <pre>
+   * myFrame.SetID(ID3FID_TITLE);
+   * </pre>
+   *
+   * @param id The type of frame this frame should be set to
+   */
   void        SetID(ID3_FrameID id);
+
+  /** 
+   * Useful in conjunction with the @ref ID3_Tag#GetFrame method, this
+   * method returns the type of frame that the object represents, just so you
+   * know what to expect.
+   *
+   * @returns The type, or id, of the frame
+   */
   ID3_FrameID GetID(void) const;
+
+  /**
+   * This is a go-between method that returns a pointer to one of the frame's
+   * internal fields.  A list of fields that are in given frames appears in
+   * <id3/field.cpp>.  This method returns a reference to the field in
+   * question, so that the result can be used as though it were a field
+   * itself...
+   *
+   * <pre>
+   * ID3_TextEnc enc;
+   * enc = (ID3_TextEnc) myFrame.Field(ID3FN_TEXTENC).Get();
+   * </pre>
+   *
+   * @param name The name of the field to be retrieved
+   * @returns A reference to the desired field
+   */
   ID3_Field  &Field(ID3_FieldID name) const;
   
 private:
@@ -56,6 +129,11 @@ protected:
 ;
 
 // $Log$
+// Revision 1.6  1999/11/15 20:18:11  scott
+// Made ID3_Tag a friend of ID3_Frame to allow access to the private
+// members in ID3_Frame.  Made variable names more descriptive.
+// Added const qualifier to appropriate methods.
+//
 // Revision 1.5  1999/11/04 04:32:11  scott
 // Initial revision
 //
