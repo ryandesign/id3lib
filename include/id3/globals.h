e9ab2770c8c97bc9cd2cd163e1fb34b216ecdb99
scott <>
1999-12-26 00:23:55 +0000

    (ID3_FieldID): Reformatted. (ID3_FrameID): Reformatted.  Commented descriptions from www.id3.org. Renamed some frame id's: ID3FID_KEY -> ID3FID_INITIALKEY, ID3FID_BEATSPERMINUTE -> ID3FID_BPM, ID3FID_OWNER -> ID3FID_FILEOWNER. Added new frame id's: ID3FID_AUDIOCRYPTO (AENC/CRA), ID3FID_COMMERCIAL (COMR/), ID3FID_EQUALIZATION (EQUA/EQU), ID3FID_EVENTTIMING (ETCO/ETC), ID3FID_CDID (MCDI/MCI), ID3FID_MPEGLOOKUP (MLLT/MLL), ID3FID_OWNERSHIP (OWNE/), ID3FID_POSITIONSYNC (POSS/), ID3FID_PRIVATE (PRIV/) ID3FID_BUFFERSIZE (RBUF/BUF), ID3FID_VOLUMEADJ (RVAD/RVA), ID3FID_SYNCEDLYRICS (SYLT/SLT), ID3FID_SYNCEDTEMPO (SYTC/STC), ID3FID_METACRYPTO (/CRM).

--- include/id3/globals.h
+++ include/id3/globals.h
@@ -52,52 +52,29 @@ enum ID3_TextEnc
  */
 enum ID3_FieldID
 {
-  /// No field
-  ID3FN_NOFIELD = 0,
-  /// Text encoding (unicode or ASCII)
-  ID3FN_TEXTENC,
-  /// Text field
-  ID3FN_TEXT,
-  /// A URL
-  ID3FN_URL,
-  /// Data field
-  ID3FN_DATA,
-  /// Description field
-  ID3FN_DESCRIPTION,
-  /// Owner field
-  ID3FN_OWNER,
-  /// Email field
-  ID3FN_EMAIL,
-  /// Rating field
-  ID3FN_RATING,
-  /// Filename field
-  ID3FN_FILENAME,
-  /// Language field
-  ID3FN_LANGUAGE,
-  /// Picture type field
-  ID3FN_PICTURETYPE,
-  /// Image format field
-  ID3FN_IMAGEFORMAT,
-  /// Mimetype field
-  ID3FN_MIMETYPE,
-  /// Counter field
-  ID3FN_COUNTER,
-  /// Symbol field
-  ID3FN_SYMBOL,
-  /// Volume adjustment field
-  ID3FN_VOLUMEADJ,
-  /// Number of bits field
-  ID3FN_NUMBITS,
-  /// Volume chage on the right channel
-  ID3FN_VOLCHGRIGHT,
-  /// Volume chage on the left channel
-  ID3FN_VOLCHGLEFT,
-  /// Peak volume on the right channel
-  ID3FN_PEAKVOLRIGHT,
-  /// Peak volume on the left channel
-  ID3FN_PEAKVOLLEFT,
-  /// Last field placeholder
-  ID3FN_LASTFIELDID
+  ID3FN_NOFIELD = 0,    /// No field
+  ID3FN_TEXTENC,        /// Text encoding (unicode or ASCII)
+  ID3FN_TEXT,           /// Text field
+  ID3FN_URL,            /// A URL
+  ID3FN_DATA,           /// Data field
+  ID3FN_DESCRIPTION,    /// Description field
+  ID3FN_OWNER,          /// Owner field
+  ID3FN_EMAIL,          /// Email field
+  ID3FN_RATING,         /// Rating field
+  ID3FN_FILENAME,       /// Filename field
+  ID3FN_LANGUAGE,       /// Language field
+  ID3FN_PICTURETYPE,    /// Picture type field
+  ID3FN_IMAGEFORMAT,    /// Image format field
+  ID3FN_MIMETYPE,       /// Mimetype field
+  ID3FN_COUNTER,        /// Counter field
+  ID3FN_SYMBOL,         /// Symbol field
+  ID3FN_VOLUMEADJ,      /// Volume adjustment field
+  ID3FN_NUMBITS,        /// Number of bits field
+  ID3FN_VOLCHGRIGHT,    /// Volume chage on the right channel
+  ID3FN_VOLCHGLEFT,     /// Volume chage on the left channel
+  ID3FN_PEAKVOLRIGHT,   /// Peak volume on the right channel
+  ID3FN_PEAKVOLLEFT,    /// Peak volume on the left channel
+  ID3FN_LASTFIELDID     /// Last field placeholder
 };
 
 /**
@@ -105,128 +82,82 @@ enum ID3_FieldID
  */
 enum ID3_FrameID
 {
-  /// No known frame
-  ID3FID_NOFRAME        = 0,
-  /// Original album
-  ID3FID_ORIGALBUM,
-  /// Publisher
-  ID3FID_PUBLISHER,
-  /// Encoded by
-  ID3FID_ENCODEDBY,
-  /// Encoder settings
-  ID3FID_ENCODERSETTINGS,
-  /// Original filename
-  ID3FID_ORIGFILENAME,
-  /// Language
-  ID3FID_LANGUAGE,
-  /// Which part in a set of albums
-  ID3FID_PARTINSET,
-  /// Date
-  ID3FID_DATE,
-  /// Time
-  ID3FID_TIME,
-  /// Recording dates
-  ID3FID_RECORDINGDATES,
-  /// Media type
-  ID3FID_MEDIATYPE,
-  /// File type
-  ID3FID_FILETYPE,
-  /// Net radio station
-  ID3FID_NETRADIOSTATION,
-  /// Net radio owner
-  ID3FID_NETRADIOOWNER,
-  /// Lyricist
-  ID3FID_LYRICIST,
-  /// Original artist
-  ID3FID_ORIGARTIST,
-  /// Original lyricist
-  ID3FID_ORIGLYRICIST,
-  /// Subtitle
-  ID3FID_SUBTITLE,
-  /// Mix artist
-  ID3FID_MIXARTIST,
-  /// User text
-  ID3FID_USERTEXT,
-  /// Content group
-  ID3FID_CONTENTGROUP,
-  /// Title
-  ID3FID_TITLE,
-  /// Lead artist
-  ID3FID_LEADARTIST,
-  /// Band
-  ID3FID_BAND,
-  /// Album
-  ID3FID_ALBUM,
-  /// Year
-  ID3FID_YEAR,
-  /// Conductor
-  ID3FID_CONDUCTOR,
-  /// Composer
-  ID3FID_COMPOSER,
-  /// Copyright
-  ID3FID_COPYRIGHT,
-  /// Content-type
-  ID3FID_CONTENTTYPE,
-  /// Track number
-  ID3FID_TRACKNUM,
-  /// Comment
-  ID3FID_COMMENT,
-  /// WWW Audio file
-  ID3FID_WWWAUDIOFILE,
-  /// WWW artist
-  ID3FID_WWWARTIST,
-  /// WWW audio source
-  ID3FID_WWWAUDIOSOURCE,
-  /// WWW commercial info
-  ID3FID_WWWCOMMERCIALINFO,
-  /// WWW copyright
-  ID3FID_WWWCOPYRIGHT,
-  /// WWW publisher
-  ID3FID_WWWPUBLISHER,
-  /// WWW payment
-  ID3FID_WWWPAYMENT,
-  /// WWW radio page
-  ID3FID_WWWRADIOPAGE,
-  /// WWW user
-  ID3FID_WWWUSER,
-  /// Involved people list
-  ID3FID_INVOLVEDPEOPLE,
-  /// Unsynchronised lyrics
-  ID3FID_UNSYNCEDLYRICS,
-  /// Picture
-  ID3FID_PICTURE,
-  /// General object
-  ID3FID_GENERALOBJECT,
-  /// Unique file id
-  ID3FID_UNIQUEFILEID,
-  /// Play counter
-  ID3FID_PLAYCOUNTER,
-  /// Popularimeter
-  ID3FID_POPULARIMETER,
-  /// Grouping reg
-  ID3FID_GROUPINGREG,
-  /// Cryptography reg
-  ID3FID_CRYPTOREG,
-  /// Linked Information
-  ID3FID_LINKEDINFO,
-  /// BPM (Beats Per Minute)
-  ID3FID_BEATSPERMINUTE,
-  /// Playlist delay
-  ID3FID_PLAYLISTDELAY,
-  /// Initial key
-  ID3FID_KEY,
-  /// Song length
-  ID3FID_SONGLEN,
-  /// Original year
-  ID3FID_ORIGYEAR,
-  /// File owner/licensee
-  ID3FID_OWNER,
-  /// Size
-  ID3FID_SIZE,
-  /// ISRC
-  ID3FID_ISRC,
-  /// Terms of use
-  ID3FID_TERMSOFUSE
+  /* ???? */ ID3FID_NOFRAME = 0,       /// No known frame
+  /* AENC */ ID3FID_AUDIOCRYPTO,       /// Audio encryption
+  /* APIC */ ID3FID_PICTURE,           /// Attached picture
+  /* COMM */ ID3FID_COMMENT,           /// Comments
+  /* COMR */ ID3FID_COMMERCIAL,        /// Commercial frame
+  /* ENCR */ ID3FID_CRYPTOREG,         /// Encryption method registration
+  /* EQUA */ ID3FID_EQUALIZATION,      /// Equalization
+  /* ETCO */ ID3FID_EVENTTIMING,       /// Event timing codes
+  /* GEOB */ ID3FID_GENERALOBJECT,     /// General encapsulated object
+  /* GRID */ ID3FID_GROUPINGREG,       /// Group identification registration
+  /* IPLS */ ID3FID_INVOLVEDPEOPLE,    /// Involved people list
+  /* LINK */ ID3FID_LINKEDINFO,        /// Linked information
+  /* MCDI */ ID3FID_CDID,              /// Music CD identifier
+  /* MLLT */ ID3FID_MPEGLOOKUP,        /// MPEG location lookup table
+  /* OWNE */ ID3FID_OWNERSHIP,         /// Ownership frame
+  /* PRIV */ ID3FID_PRIVATE,           /// Private frame
+  /* PCNT */ ID3FID_PLAYCOUNTER,       /// Play counter
+  /* POPM */ ID3FID_POPULARIMETER,     /// Popularimeter
+  /* POSS */ ID3FID_POSITIONSYNC,      /// Position synchronisation frame
+  /* RBUF */ ID3FID_BUFFERSIZE,        /// Recommended buffer size
+  /* RVAD */ ID3FID_VOLUMEADJ,         /// Relative volume adjustment
+  /* RVRB */ ID3FID_REVERB,            /// Reverb
+  /* SYLT */ ID3FID_SYNCEDLYRICS,      /// Synchronized lyric/text
+  /* SYTC */ ID3FID_SYNCEDTEMPO,       /// Synchronized tempo codes
+  /* TALB */ ID3FID_ALBUM,             /// Album/Movie/Show title
+  /* TBPM */ ID3FID_BPM,               /// BPM (beats per minute)
+  /* TCOM */ ID3FID_COMPOSER,          /// Composer
+  /* TCON */ ID3FID_CONTENTTYPE,       /// Content type
+  /* TCOP */ ID3FID_COPYRIGHT,         /// Copyright message
+  /* TDAT */ ID3FID_DATE,              /// Date
+  /* TDLY */ ID3FID_PLAYLISTDELAY,     /// Playlist delay
+  /* TENC */ ID3FID_ENCODEDBY,         /// Encoded by
+  /* TEXT */ ID3FID_LYRICIST,          /// Lyricist/Text writer
+  /* TFLT */ ID3FID_FILETYPE,          /// File type
+  /* TIME */ ID3FID_TIME,              /// Time
+  /* TIT1 */ ID3FID_CONTENTGROUP,      /// Content group description
+  /* TIT2 */ ID3FID_TITLE,             /// Title/songname/content description
+  /* TIT3 */ ID3FID_SUBTITLE,          /// Subtitle/Description refinement
+  /* TKEY */ ID3FID_INITIALKEY,        /// Initial key
+  /* TLAN */ ID3FID_LANGUAGE,          /// Language(s)
+  /* TLEN */ ID3FID_SONGLEN,           /// Length
+  /* TMED */ ID3FID_MEDIATYPE,         /// Media type
+  /* TOAL */ ID3FID_ORIGALBUM,         /// Original album/movie/show title
+  /* TOFN */ ID3FID_ORIGFILENAME,      /// Original filename
+  /* TOLY */ ID3FID_ORIGLYRICIST,      /// Original lyricist(s)/text writer(s)
+  /* TOPE */ ID3FID_ORIGARTIST,        /// Original artist(s)/performer(s)
+  /* TORY */ ID3FID_ORIGYEAR,          /// Original release year
+  /* TOWN */ ID3FID_FILEOWNER,         /// File owner/licensee
+  /* TPE1 */ ID3FID_LEADARTIST,        /// Lead performer(s)/Soloist(s)
+  /* TPE2 */ ID3FID_BAND,              /// Band/orchestra/accompaniment
+  /* TPE3 */ ID3FID_CONDUCTOR,         /// Conductor/performer refinement
+  /* TPE4 */ ID3FID_MIXARTIST,         /// Interpreted, remixed, or otherwise modified by
+  /* TPOS */ ID3FID_PARTINSET,         /// Part of a set
+  /* TPUB */ ID3FID_PUBLISHER,         /// Publisher
+  /* TRCK */ ID3FID_TRACKNUM,          /// Track number/Position in set
+  /* TRDA */ ID3FID_RECORDINGDATES,    /// Recording dates
+  /* TRSN */ ID3FID_NETRADIOSTATION,   /// Internet radio station name
+  /* TRSO */ ID3FID_NETRADIOOWNER,     /// Internet radio station owner
+  /* TSIZ */ ID3FID_SIZE,              /// Size
+  /* TSRC */ ID3FID_ISRC,              /// ISRC (international standard recording code)
+  /* TSSE */ ID3FID_ENCODERSETTINGS,   /// Software/Hardware and settings used for encoding
+  /* TXXX */ ID3FID_USERTEXT,          /// User defined text information
+  /* TYER */ ID3FID_YEAR,              /// Year
+  /* UFID */ ID3FID_UNIQUEFILEID,      /// Unique file identifier
+  /* USER */ ID3FID_TERMSOFUSE,        /// Terms of use
+  /* USLT */ ID3FID_UNSYNCEDLYRICS,    /// Unsynchronized lyric/text transcription
+  /* WCOM */ ID3FID_WWWCOMMERCIALINFO, /// Commercial information
+  /* WCOP */ ID3FID_WWWCOPYRIGHT,      /// Copyright/Legal infromation
+  /* WOAF */ ID3FID_WWWAUDIOFILE,      /// Official audio file webpage
+  /* WOAR */ ID3FID_WWWARTIST,         /// Official artist/performer webpage
+  /* WOAS */ ID3FID_WWWAUDIOSOURCE,    /// Official audio source webpage
+  /* WORS */ ID3FID_WWWRADIOPAGE,      /// Official internet radio station homepage
+  /* WPAY */ ID3FID_WWWPAYMENT,        /// Payment
+  /* WPUB */ ID3FID_WWWPUBLISHER,      /// Official publisher webpage
+  /* WXXX */ ID3FID_WWWUSER,           /// User defined URL link
+  /*      */ ID3FID_METACRYPTO         /// Encrypted meta frame (id3v2.2.x)
 };
 
 #endif /* __GLOBALS_H__ */
