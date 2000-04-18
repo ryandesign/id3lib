348c929f9c18da0d1d96e700693d7b51287dc2f3
eldamitri <>
2000-04-18 22:12:51 +0000

    Moved tag.cpp from src/id3/ to src/

--- src/tag.cpp
+++ src/tag.cpp
@@ -397,6 +397,10 @@ ID3_Tag::operator=( const ID3_Tag &rTag )
 }
 
 // $Log$
+// Revision 1.14  2000/04/05 05:21:15  eldamitri
+// Updated initial comment information to reflect license, copyright
+// change.
+//
 // Revision 1.13  2000/01/04 15:42:49  eldamitri
 // For compilation with gcc 2.95.2 and better compatibility with ANSI/ISO
 // standard C++, updated, rearranged, and removed (where necessary)
