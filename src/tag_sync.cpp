4adebcd8cc37274d975b77f43af62c6cd37e7cba
eldamitri <>
2000-04-18 22:14:13 +0000

    Moved tag_sync.cpp from src/id3/ to src/

--- src/tag_sync.cpp
+++ src/tag_sync.cpp
@@ -112,6 +112,10 @@ void ID3_Tag::UnSync(uchar *pDestData, luint nDestSize, uchar *pSrcData, luint n
 
 
 // $Log$
+// Revision 1.10  2000/04/05 05:21:15  eldamitri
+// Updated initial comment information to reflect license, copyright
+// change.
+//
 // Revision 1.9  2000/01/04 15:42:49  eldamitri
 // For compilation with gcc 2.95.2 and better compatibility with ANSI/ISO
 // standard C++, updated, rearranged, and removed (where necessary)
