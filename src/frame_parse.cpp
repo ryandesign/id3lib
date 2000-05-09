77559e818693ccd152700c01b9e4b71920b7236f
eldamitri <>
2000-05-09 13:18:20 +0000

    (Parse): New implementation

--- src/frame_parse.cpp
+++ src/frame_parse.cpp
@@ -31,6 +31,95 @@
 #include <config.h>
 #endif
 
+size_t ID3_Frame::Parse(const uchar * const buffer, luint size) 
+{ 
+  __bad_parse = false;  
+  size_t hdr_size = __hdr.Parse(buffer, size);  
+  size_t data_size = __hdr.GetDataSize() - __hdr.GetExtrasSize();  
+  
+  if (!hdr_size)  
+  {  
+    return 0;  
+  }  
+  
+  // data is the part of the buffer that appears after the header  
+  const uchar * data = &buffer[hdr_size]; 
+  uchar* expanded_data = NULL;
+      
+  // expand out the data if it's compressed 
+  if (__hdr.GetCompression())
+  {  
+    unsigned long expanded_size = __hdr.GetExpandedSize();  
+    expanded_data = new uchar[expanded_size];  
+        
+    uncompress(expanded_data, &expanded_size, data, data_size);  
+    data = expanded_data; 
+    data_size = expanded_size; 
+  }  
+      
+  // set the type of frame based on the parsed header  
+  this->_ClearFields(); 
+  this->_InitFields(); 
+  try  
+  {  
+    ID3_TextEnc enc = ID3TE_ASCII;  // set the default encoding 
+    size_t remainder = data_size;   // how many bytes remain to be parsed 
+    ID3_V2Spec spec = this->GetSpec(); 
+    // parse the frame's fields  
+    for (ID3_Field** fi = __fields; fi != __fields + __num_fields; fi++)
+    {
+      if (!*fi)
+      {
+        // Ack!  Why is the field NULL?  Log this...
+        continue;
+      }
+      if (remainder == 0) 
+      { 
+        // there's no remaining data to parse! 
+        __bad_parse = true; 
+        break; 
+      } 
+ 
+      if (!(*fi)->InScope(spec)) 
+      { 
+        // this field isn't in scope, so don't attempt to parse into it 
+        continue; 
+      }
+      
+      size_t frame_size = (*fi)->Parse(data, remainder); 
+      
+      if (0 == frame_size) 
+      { 
+        // nothing to parse!  ack!  parse error... 
+        __bad_parse = true; 
+        break; 
+      } 
+ 
+      if ((*fi)->GetID() == ID3FN_TEXTENC)  
+      {  
+        enc = static_cast<ID3_TextEnc>((*fi)->Get());  
+      }  
+      
+      data += frame_size; 
+      remainder -= MIN(frame_size, remainder); 
+    }  
+  }  
+  catch (...)  
+  {  
+    // TODO: log this!
+    //cerr << "*** parsing error!" << endl;  
+    // There's been an error in the parsing of the frame.  
+    __bad_parse = true;  
+  }  
+      
+  __changed = false;
+
+  delete [] expanded_data;
+ 
+  return MIN(hdr_size + data_size, size);  
+} 
+
+/*
 void ID3_Frame::Parse(const uchar *buffer, luint size)
 {
   luint i;
@@ -54,3 +143,4 @@ void ID3_Frame::Parse(const uchar *buffer, luint size)
   
   return ;
 }
+*/
