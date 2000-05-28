d2412045272c17fd8ffd135e06fa07dafd2fb3ec
eldamitri <>
2000-05-28 03:51:23 +0000

    Added.

---
+++ examples/test_pic.cpp
@@ -0,0 +1,76 @@
+// $Id$
+
+#include <iostream.h>
+#include <getopt.h>
+#include <stdlib.h>
+
+#include <id3/tag.h>
+#include <id3/misc_support.h>
+
+int main( int argc, char *argv[])
+{
+  try
+  {
+    ID3_Tag tag;
+    ID3_Frame frame;
+
+    tag.Link("test-230-picture.tag");
+    tag.Strip(ID3TT_ALL);
+    tag.Update();
+
+    tag.Clear();
+    /*
+    frame.SetID(ID3FID_TITLE);
+    frame.Field(ID3FN_TEXT).Set("Aquarium");
+    tag.AddFrame(frame);
+
+    frame.SetID(ID3FID_CONTENTGROUP);
+    frame.Field(ID3FN_TEXT).Set("Short fraction of 'Carnival of the Animals: A Grand Zoological Fantasy'");
+    tag.AddFrame(frame);
+
+    frame.SetID(ID3FID_COMPOSER);
+    frame.Field(ID3FN_TEXT).Set("Camille Saint-Saëns");
+    tag.AddFrame(frame);
+
+    frame.SetID(ID3FID_BAND);
+    frame.Field(ID3FN_TEXT).Set("Slovakia Radio Symphony Orchestra");
+    tag.AddFrame(frame);
+
+    frame.SetID(ID3FID_CONDUCTOR);
+    frame.Field(ID3FN_TEXT).Set("Ondrej Lenárd");
+    tag.AddFrame(frame);
+
+    frame.SetID(ID3FID_COPYRIGHT);
+    frame.Field(ID3FN_TEXT).Set("1996 HNH international Ltd.");
+    tag.AddFrame(frame);
+
+    frame.SetID(ID3FID_CONTENTTYPE);
+    frame.Field(ID3FN_TEXT).Set("(32)");
+    tag.AddFrame(frame);
+
+    frame.SetID(ID3FID_INVOLVEDPEOPLE);
+    frame.Field(ID3FN_TEXT).Add("Producer");
+    frame.Field(ID3FN_TEXT).Add("Martin Sauer");
+    frame.Field(ID3FN_TEXT).Add("Piano");
+    frame.Field(ID3FN_TEXT).Add("Peter Toperczer");
+    tag.AddFrame(frame);
+
+    frame.SetID(ID3FID_PICTURE);
+    frame.Field(ID3FN_MIMETYPE).Set("image/jpeg");
+    frame.Field(ID3FN_PICTURETYPE).Set(11);
+    frame.Field(ID3FN_DESCRIPTION).Set("B/W picture of Saint-Saëns");
+    tag.AddFrame(frame);
+    */
+    tag.SetPadding(false);
+    tag.SetUnsync(true);
+    tag.Update(ID3TT_ID3V2);
+  }   
+  catch(const ID3_Error& err)
+  {
+    cout << endl;
+    cout << err.GetErrorFile() << " (" << err.GetErrorLine() << "): "
+         << err.GetErrorType() << ": " << err.GetErrorDesc() << endl;
+  }
+
+  return 0;
+}
