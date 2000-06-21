621e33d053a649294a922bd9d3747727dc0744bc
eldamitri <>
2000-06-21 03:30:13 +0000

    Initial revision.

---
+++ examples/test_remove.cpp
@@ -0,0 +1,50 @@
+// $Id$
+
+#include <iostream.h>
+#include <id3/tag.h>
+#include <id3/misc_support.h>
+
+int main( int argc, char *argv[])
+{
+  try
+  {
+    ID3_Tag tag;
+    ID3_Frame frame;
+
+    tag.Link("test-remove.tag");
+    tag.Strip(ID3TT_ALL);
+    tag.Update();
+    tag.Clear();
+
+    tag.Clear();
+    frame.SetID(ID3FID_TITLE);
+    frame.Field(ID3FN_TEXT).Set("Test title");
+    tag.AddFrame(frame);
+
+    frame.SetID(ID3FID_COMPOSER);
+    frame.Field(ID3FN_TEXT).Set("Test composer");
+    tag.AddFrame(frame);
+
+    frame.SetID(ID3FID_BAND);
+    frame.Field(ID3FN_TEXT).Set("Test band");
+    tag.AddFrame(frame);
+
+    frame.SetID(ID3FID_CONDUCTOR);
+    frame.Field(ID3FN_TEXT).Set("Test conductor");
+    tag.AddFrame(frame);
+
+    tag.SetPadding(false);
+    tag.Update(ID3TT_ID3V2);
+
+    cerr << "removed " << ID3_RemoveArtists(&tag) << " artist frames" << endl;
+    tag.Update();
+  }   
+  catch(const ID3_Error& err)
+  {
+    cout << endl;
+    cout << err.GetErrorFile() << " (" << err.GetErrorLine() << "): "
+         << err.GetErrorType() << ": " << err.GetErrorDesc() << endl;
+  }
+
+  return 0;
+}
