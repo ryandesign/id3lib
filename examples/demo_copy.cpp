ba2f31365c24067d7e65464eeaf3deabf8bb7ab4
eldamitri <>
2000-06-23 03:53:56 +0000

    Initial revision.

---
+++ examples/demo_copy.cpp
@@ -0,0 +1,154 @@
+// $Id$
+//
+//  The authors have released ID3Lib as Public Domain (PD) and claim no
+//  copyright, patent or other intellectual property protection in this work.
+//  This means that it may be modified, redistributed and used in commercial
+//  and non-commercial software and hardware without restrictions.  ID3Lib is
+//  distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either
+//  express or implied.
+//  
+//  The ID3Lib authors encourage improvements and optimisations to be sent to
+//  the ID3Lib coordinator, currently Dirk Mahoney (dirk@id3.org).  Approved
+//  submissions may be altered, and will be included and released under these
+//  terms.
+
+#include <iostream.h>
+#include <id3/tag.h>
+#include <getopt.h>
+
+#define VERSION_NUMBER "$Revision$"
+
+void PrintUsage(char *sName)
+{
+  cout << "Usage: " << sName << " [OPTION]... SOURCE DEST" << endl;
+  cout << "Copy the tag(s) from SOURCE to DEST." << endl;
+  cout << endl;
+  cout << "  -1, --v1tag     Render only the id3v1 tag" << endl;
+  cout << "  -2, --v2tag     Render only the id3v2 tag" << endl;
+  cout << "  -h, --help      Display this help and exit" << endl;
+  cout << "  -v, --version   Display version information and exit" << endl;
+  cout << endl;
+}
+
+void PrintVersion(char *sName)
+{
+  size_t nIndex;
+  cout << sName << " ";
+  for (nIndex = 0; nIndex < strlen(VERSION_NUMBER); nIndex++)
+  {
+    if (VERSION_NUMBER[nIndex] == ' ') 
+    {
+      break;
+    }
+  }
+  nIndex++;
+  for (; nIndex < strlen (VERSION_NUMBER); nIndex++)
+  {
+    if (VERSION_NUMBER[nIndex] == ' ') 
+    {
+      break;
+    }
+    cout << VERSION_NUMBER[nIndex];
+  }
+  cout << endl;
+  cout << "Uses " << ID3LIB_FULL_NAME << endl << endl;
+
+  cout << "This program copies tags from one file to another" << endl;
+}
+
+void DisplayTags(ostream &os, luint nTags)
+{
+  if (!((nTags & ID3TT_ID3V1) || (nTags & ID3TT_ID3V2)))
+  {
+    os << "no tag";
+  }
+  if (nTags & ID3TT_ID3V1)
+  {
+    os << "v1";
+  }
+  if ((nTags & ID3TT_ID3V1) && (nTags & ID3TT_ID3V2))
+  {
+    os << " and ";
+  }
+  if (nTags & ID3TT_ID3V2)
+  {
+    os << "v2";
+  }
+}
+
+int main( unsigned int argc, char *argv[])
+{
+  int ulFlag = ID3TT_ID3;
+  int iOpt;
+  bool bError = false;
+  bool bStrip = false;
+  bool bPadding = false;
+  while (true)
+  {
+    int option_index = 0;
+    int iLongOpt = 0;
+    static struct option long_options[] = 
+    { 
+      { "v1tag",   no_argument, &iLongOpt, '1' },
+      { "v2tag",   no_argument, &iLongOpt, '2' },
+      { "version", no_argument, &iLongOpt, 'v' },
+      { "help",    no_argument, &iLongOpt, 'h' },
+      { 0, 0, 0, 0 }
+    };
+    iOpt = getopt_long (argc, argv, "12vh", long_options, &option_index);
+
+    if (iOpt == -1)
+      break;
+
+    if (iOpt == 0) iOpt = iLongOpt;
+
+    switch (iOpt)
+    {
+      case '1': ulFlag = ID3TT_ID3V1;        break;
+      case '2': ulFlag = ID3TT_ID3V2;        break;
+      case 'v': PrintVersion(argv[0]);  exit (0);
+      case 'h': PrintUsage(argv[0]);    exit (0);
+      case '?': bError = true;          break;
+      default:
+        cout << "?? getopt returned character code " << iOpt << " ??" << endl;
+    }
+  }
+  if (bError)
+  {
+    cout << "Try `" << argv[0] << " --help' for more information." << endl;
+  }
+  else 
+    for (size_t nIndex = optind; nIndex < argc; nIndex++)
+    {
+      try
+      {
+        ID3_Tag myTag;
+
+        cout << "Parsing " << argv[nIndex] << ": ";
+
+        myTag.Clear();
+        myTag.Link(argv[nIndex], ID3TT_ALL);
+
+        nIndex++;
+        cout << "done.  Copying to " << argv[nIndex] << ": ";
+        
+        myTag.Link(argv[nIndex], ID3TT_NONE);
+        myTag.Update(ulFlag);
+        
+        cout << "done" << endl;
+        cerr << "numframes = " << myTag.NumFrames() << endl;
+      }
+
+      catch(ID3_Error& err)
+      {
+        cout << endl;
+        cout << err.GetErrorFile() << " (" << err.GetErrorLine() << "): "
+             << err.GetErrorType() << ": " << err.GetErrorDesc() << endl;
+        exit(1);
+      }
+    }
+
+  return 0;
+}
+
+
