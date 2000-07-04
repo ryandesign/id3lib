278f727535bcde1964fc314f626b3278738a6c39
eldamitri <>
2000-07-04 02:46:51 +0000

    (main): Tests the ability to remove frames with NULL descriptions.

--- examples/test_remove.cpp
+++ examples/test_remove.cpp
@@ -4,6 +4,31 @@
 #include <id3/tag.h>
 #include <id3/misc_support.h>
 
+typedef const char* LPCTSTR;
+
+/* CSharedTag is a wrapper I made for some features I need */
+/* LPCTSTR means const char * */
+size_t RemoveFrame(ID3_Tag& pTag, ID3_FrameID fID, LPCTSTR sDescription)
+{
+  size_t nCount = 0;
+  const ID3_Frame * frame = NULL;
+
+  do {
+    if (!sDescription)
+      frame = pTag.Find(fID);
+    else
+      frame = pTag.Find(fID, ID3FN_DESCRIPTION, sDescription);
+
+    if (frame) {
+      /* pTag is an ID3_Tag */
+      delete pTag.RemoveFrame(frame);
+      nCount++;
+    }
+  } while (frame != NULL);
+
+  return nCount;
+}
+
 int main( int argc, char *argv[])
 {
   try
@@ -31,11 +56,26 @@ int main( int argc, char *argv[])
     frame.Field(ID3FN_TEXT).Set("Test conductor");
     tag.AddFrame(frame);
 
+    frame.SetID(ID3FID_COMMENT);
+    frame.Field(ID3FN_LANGUAGE).Set("eng");
+    frame.Field(ID3FN_TEXT).Set("Test comment");
+    frame.Field(ID3FN_DESCRIPTION).Set("A Description");
+    tag.AddFrame(frame);
+
+    frame.SetID(ID3FID_COMMENT);
+    frame.Field(ID3FN_LANGUAGE).Set("eng");
+    frame.Field(ID3FN_TEXT).Set("Test comment 2");
+    frame.Field(ID3FN_DESCRIPTION).Set("");
+    tag.AddFrame(frame);
+
     tag.SetPadding(false);
     tag.Update(ID3TT_ID3V2);
 
     cerr << "removed " << ID3_RemoveArtists(&tag) << " artist frames" << endl;
+    tag.Update();
     cerr << "removed " << ID3_RemoveTitles(&tag) << " title frames" << endl;
+    tag.Update();
+    cerr << "removed " << RemoveFrame(tag, ID3FID_COMMENT, "") << " descriptionless comment frames" << endl;
     tag.Update();
   }   
   catch(const ID3_Error& err)
