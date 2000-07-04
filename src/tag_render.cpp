700cec5c4ae5b607109bf0bfb31d1ad053288ad5
eldamitri <>
2000-07-04 22:54:36 +0000

    (RenderV2): Added (Render): Now calls either RenderV1 or RenderV2 based on second parameter (RenderV1ToHandle): Moved to tag_file.cpp (RenderV2ToHandle): Moved to tag_file.cpp

--- src/tag_render.cpp
+++ src/tag_render.cpp
@@ -35,19 +35,98 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
-#include <fstream.h>
 #include <memory.h>
 #include "tag.h"
 #include "misc_support.h"
 #include "utils.h"
 
-#ifdef  MAXPATHLEN
-#  define ID3_PATH_LENGTH   (MAXPATHLEN + 1)
-#elif   defined (PATH_MAX)
-#  define ID3_PATH_LENGTH   (PATH_MAX + 1)
-#else   /* !MAXPATHLEN */
-#  define ID3_PATH_LENGTH   (2048 + 1)
-#endif  /* !MAXPATHLEN && !PATH_MAX */
+/** Renders an id3v1.1 version of the tag into the supplied buffer.
+ ** 
+ ** @return The actual number of the bytes of the buffer used to store the
+ **         tag (should always be 128)
+ ** @param  buffer The buffer that will contain the id3v1.1 tag.
+ **/
+size_t RenderV1(const ID3_Tag& tag, uchar *buffer)
+{
+  // Sanity check our buffer
+  if (NULL == buffer)
+  {
+    ID3_THROW(ID3E_NoBuffer);
+  }
+
+  // pCur is used to mark where to next write in the buffer
+  // sTemp is used as a temporary string pointer for functions that return
+  //  dynamically created strings
+  char* pCur = (char *) buffer;
+  char* sTemp = NULL;
+
+  // The default char for a v1 tag is null
+  memset(buffer, '\0', ID3_V1_LEN);
+
+  // Write the TAG identifier
+  strncpy(pCur, "TAG", ID3_V1_LEN_ID);
+  pCur = &pCur[ID3_V1_LEN_ID];
+
+  // Write the TITLE
+  sTemp = ID3_GetTitle(&tag);
+  if (sTemp != NULL)
+  {
+    strncpy(pCur, sTemp, ID3_V1_LEN_TITLE);
+    delete [] sTemp;
+  }
+  pCur = &pCur[ID3_V1_LEN_TITLE];
+
+  // Write the ARTIST
+  sTemp = ID3_GetArtist(&tag);
+  if (sTemp != NULL)
+  {
+    strncpy(pCur, sTemp, ID3_V1_LEN_ARTIST);
+    delete [] sTemp;
+  }
+  pCur = &pCur[ID3_V1_LEN_ARTIST];
+
+  // Write the ALBUM
+  sTemp = ID3_GetAlbum(&tag);
+  if (sTemp != NULL)
+  {
+    strncpy(pCur, sTemp, ID3_V1_LEN_ALBUM);
+    delete [] sTemp;
+  }
+  pCur = &pCur[ID3_V1_LEN_ALBUM];
+
+  // Write the YEAR
+  sTemp = ID3_GetYear(&tag);
+  if (sTemp != NULL)
+  {
+    strncpy(pCur, sTemp, ID3_V1_LEN_YEAR);
+    delete [] sTemp;
+  }
+  pCur = &pCur[ID3_V1_LEN_YEAR];
+
+  // Write the COMMENT
+  sTemp = ID3_GetComment(&tag);
+  if (sTemp != NULL)
+  {
+    strncpy(pCur, sTemp, ID3_V1_LEN_COMMENT);
+    delete [] sTemp;
+  }
+  pCur = &pCur[ID3_V1_LEN_COMMENT];
+
+  // Write the TRACK, if it isn't 0
+  luint nTrack = ID3_GetTrackNum(&tag);
+  if (0 != nTrack)
+  {
+    pCur -= 2;
+    pCur[0] = '\0';
+    pCur[1] = (uchar) nTrack;
+    pCur += 2;
+  }
+
+  // Write the GENRE
+  pCur[0] = (uchar) ID3_GetGenreNum(&tag);
+
+  return ID3_V1_LEN;
+}
 
 size_t RenderFrames(uchar* buffer, const ID3_Elem* cur)
 {
@@ -78,7 +157,7 @@ size_t RenderFrames(uchar* buffer, const ID3_Elem* cur)
  **         tag
  ** @param  buffer The buffer that will contain the rendered tag.
  **/
-size_t ID3_Tag::Render(uchar *buffer) const
+size_t ID3_Tag::RenderV2(uchar *buffer) const
 {
   // There has to be at least one frame for there to be a tag...
   if (this->NumFrames() == 0)
@@ -140,10 +219,21 @@ size_t ID3_Tag::Render(uchar *buffer) const
   hdr.SetDataSize(bytesUsed - hdr_size);
   hdr.Render(buffer);
   
-  // set the flag which says that the tag hasn't changed
-  __changed = false;
-  
   return bytesUsed;
+}
+
+size_t ID3_Tag::Render(uchar* buffer, ID3_TagType tt) const
+{
+  size_t tag_bytes = 0;
+  if (tt & ID3TT_ID3V2)
+  {
+    tag_bytes = this->RenderV2(buffer);
+  }
+  else if (tt & ID3TT_ID3V1)
+  {
+    tag_bytes = RenderV1(*this, buffer);
+  }
+  return tag_bytes;
 }
 
   /** Returns an over estimate of the number of bytes required to store a
@@ -227,304 +317,6 @@ void ID3_Tag::RenderExtHeader(uchar *buffer)
 }
 
 
-  /** Renders an id3v1.1 version of the tag into the supplied buffer.
-   ** 
-   ** @return The actual number of the bytes of the buffer used to store the
-   **         tag (should always be 128)
-   ** @param  buffer The buffer that will contain the id3v1.1 tag.
-   **/
-size_t ID3_Tag::RenderV1(uchar *buffer) const
-{
-  // Sanity check our buffer
-  if (NULL == buffer)
-  {
-    ID3_THROW(ID3E_NoBuffer);
-  }
-
-  // pCur is used to mark where to next write in the buffer
-  // sTemp is used as a temporary string pointer for functions that return
-  //  dynamically created strings
-  char* pCur = (char *) buffer;
-  char* sTemp = NULL;
-
-  // The default char for a v1 tag is null
-  memset(buffer, '\0', ID3_V1_LEN);
-
-  // Write the TAG identifier
-  strncpy(pCur, "TAG", ID3_V1_LEN_ID);
-  pCur = &pCur[ID3_V1_LEN_ID];
-
-  // Write the TITLE
-  sTemp = ID3_GetTitle(this);
-  if (sTemp != NULL)
-  {
-    strncpy(pCur, sTemp, ID3_V1_LEN_TITLE);
-    delete [] sTemp;
-  }
-  pCur = &pCur[ID3_V1_LEN_TITLE];
-
-  // Write the ARTIST
-  sTemp = ID3_GetArtist(this);
-  if (sTemp != NULL)
-  {
-    strncpy(pCur, sTemp, ID3_V1_LEN_ARTIST);
-    delete [] sTemp;
-  }
-  pCur = &pCur[ID3_V1_LEN_ARTIST];
-
-  // Write the ALBUM
-  sTemp = ID3_GetAlbum(this);
-  if (sTemp != NULL)
-  {
-    strncpy(pCur, sTemp, ID3_V1_LEN_ALBUM);
-    delete [] sTemp;
-  }
-  pCur = &pCur[ID3_V1_LEN_ALBUM];
-
-  // Write the YEAR
-  sTemp = ID3_GetYear(this);
-  if (sTemp != NULL)
-  {
-    strncpy(pCur, sTemp, ID3_V1_LEN_YEAR);
-    delete [] sTemp;
-  }
-  pCur = &pCur[ID3_V1_LEN_YEAR];
-
-  // Write the COMMENT
-  sTemp = ID3_GetComment(this);
-  if (sTemp != NULL)
-  {
-    strncpy(pCur, sTemp, ID3_V1_LEN_COMMENT);
-    delete [] sTemp;
-  }
-  pCur = &pCur[ID3_V1_LEN_COMMENT];
-
-  // Write the TRACK, if it isn't 0
-  luint nTrack = ID3_GetTrackNum(this);
-  if (0 != nTrack)
-  {
-    pCur -= 2;
-    pCur[0] = '\0';
-    pCur[1] = (uchar) nTrack;
-    pCur += 2;
-  }
-
-  // Write the GENRE
-  pCur[0] = (uchar) ID3_GetGenreNum(this);
-
-  return ID3_V1_LEN;
-}
-
-void ID3_Tag::RenderV1ToHandle()
-{
-  uchar sTag[ID3_V1_LEN];
-  char sID[ID3_V1_LEN_ID];
-
-  RenderV1(sTag);
-
-  if (__file_handle == NULL)
-  {
-    // log this
-    ID3_THROW(ID3E_NoData);
-    // cerr << "*** Ack! __file_handle is null!" << endl;
-  }
-
-  if (ID3_V1_LEN > __file_size)
-  {
-    if (fseek(__file_handle, 0, SEEK_END) != 0)
-    {
-      // TODO:  This is a bad error message.  Make it more descriptive
-      ID3_THROW(ID3E_NoData);
-    }
-  }
-  else
-  {
-    // We want to check if there is already an id3v1 tag, so we can write over
-    // it.  First, seek to the beginning of any possible id3v1 tag
-    if (fseek(__file_handle, 0-ID3_V1_LEN, SEEK_END) != 0)
-    {
-      // TODO:  This is a bad error message.  Make it more descriptive
-      ID3_THROW(ID3E_NoData);
-    }
-
-    // Read in the TAG characters
-    if (fread(sID, 1, ID3_V1_LEN_ID, __file_handle) != ID3_V1_LEN_ID)
-    {
-      // TODO:  This is a bad error message.  Make it more descriptive
-      ID3_THROW(ID3E_NoData);
-    }
-
-    // If those three characters are TAG, then there's a preexisting id3v1 tag,
-    // so we should set the file cursor so we can overwrite it with a new tag.
-    if (memcmp(sID, "TAG", ID3_V1_LEN_ID) == 0)
-    {
-      if (fseek(__file_handle, 0-ID3_V1_LEN, SEEK_END) != 0)
-      {
-        // TODO:  This is a bad error message.  Make it more descriptive
-        ID3_THROW(ID3E_NoData);
-      }
-    }
-    // Otherwise, set the cursor to the end of the file so we can append on 
-    // the new tag.
-    else
-    {
-      if (fseek(__file_handle, 0, SEEK_END) != 0)
-      {
-        // TODO:  This is a bad error message.  Make it more descriptive
-        ID3_THROW(ID3E_NoData);
-      }
-    }
-  }
-
-  fwrite(sTag, sizeof(uchar), ID3_V1_LEN, __file_handle);
-  __file_tags.add(ID3TT_ID3V1);
-}
-
-void ID3_Tag::RenderV2ToHandle()
-{
-  uchar *buffer = NULL;
-  
-  if (NULL == __file_handle)
-  {
-    ID3_THROW(ID3E_NoData);
-  }
-
-  // Size() returns an over-estimate of the size needed for the tag
-  size_t tag_size = 0;
-  size_t size_est = this->Size();
-  if (size_est)
-  {
-    buffer = new uchar[size_est];
-    if (NULL == buffer)
-    {
-      ID3_THROW(ID3E_NoMemory);
-    }
-  
-    tag_size = this->Render(buffer);      
-    if (!tag_size)
-    {
-      delete [] buffer;
-      buffer = NULL;
-    }
-  }
-  
-
-  // if the new tag fits perfectly within the old and the old one
-  // actually existed (ie this isn't the first tag this file has had)
-  if ((0 == __starting_bytes && 0 == __file_size) || 
-      (tag_size == __starting_bytes))
-  {
-    fseek(__file_handle, 0, SEEK_SET);
-    if (buffer)
-    {
-      fwrite(buffer, 1, tag_size, __file_handle);
-    }
-    __starting_bytes = tag_size;
-  }
-  else
-  {
-#if !defined HAVE_MKSTEMP
-    // This section is for Windows folk
-
-    FILE *tempOut = tmpfile();
-    if (NULL == tempOut)
-    {
-      ID3_THROW(ID3E_ReadOnly);
-    }
-    
-    if (buffer)
-    {
-      fwrite(buffer, 1, tag_size, tempOut);
-    }
-    
-    fseek(__file_handle, __starting_bytes, SEEK_SET);
-    
-    uchar buffer2[BUFSIZ];
-    while (! feof(__file_handle))
-    {
-      size_t nBytes = fread(buffer2, 1, BUFSIZ, __file_handle);
-      fwrite(buffer2, 1, nBytes, tempOut);
-    }
-    
-    rewind(tempOut);
-    freopen(__file_name, "wb+", __file_handle);
-    
-    while (!feof(tempOut))
-    {
-      size_t nBytes = fread(buffer2, 1, BUFSIZ, tempOut);
-      fwrite(buffer2, 1, nBytes, __file_handle);
-    }
-    
-    fclose(tempOut);
-    
-#else
-
-    // else we gotta make a temp file, copy the tag into it, copy the
-    // rest of the old file after the tag, delete the old file, rename
-    // this new file to the old file's name and update the __file_handle
-
-    const char sTmpSuffix[] = ".XXXXXX";
-    if (strlen(__file_name) + strlen(sTmpSuffix) > ID3_PATH_LENGTH)
-    {
-      ID3_THROW_DESC(ID3E_NoFile, "filename too long");
-    }
-    char sTempFile[ID3_PATH_LENGTH];
-    strcpy(sTempFile, __file_name);
-    strcat(sTempFile, sTmpSuffix);
-    
-    int fd = mkstemp(sTempFile);
-    if (fd < 0)
-    {
-      remove(sTempFile);
-      ID3_THROW_DESC(ID3E_NoFile, "couldn't open temp file");
-    }
-
-    ofstream tmpOut(sTempFile);
-    if (!tmpOut.is_open())
-    {
-      remove(sTempFile);
-      ID3_THROW(ID3E_ReadOnly);
-    }
-    if (buffer)
-    {
-      tmpOut.write(buffer, tag_size);
-    }
-    fseek(__file_handle, __starting_bytes, SEEK_SET);
-      
-    uchar buffer2[BUFSIZ];
-    while (! feof(__file_handle))
-    {
-      size_t nBytes = fread(buffer2, 1, BUFSIZ, __file_handle);
-      tmpOut.write(buffer2, nBytes);
-    }
-      
-    tmpOut.close();
-
-    CloseFile();
-
-    remove(__file_name);
-    rename(sTempFile, __file_name);
-
-    OpenFileForWriting();
-    
-#endif
-    __starting_bytes = tag_size;
-  }
-
-  if (tag_size > 0)
-  {
-    // make sure that the v2 flag is on
-    __file_tags.add(ID3TT_ID3V2);
-  }
-  if (buffer)
-  {
-    delete [] buffer;
-  }
-    
-  return ;
-}
-
-
 #define ID3_PADMULTIPLE (2048)
 #define ID3_PADMAX  (4096)
 
@@ -534,7 +326,7 @@ size_t ID3_Tag::PaddingSize(size_t curSize) const
   luint newSize = 0;
   
   // if padding is switched off or there is no attached file
-  if (! __is_padded || __file_size == 0)
+  if (! __is_padded || !this->GetPrependedBytes())
   {
     return 0;
   }
@@ -542,14 +334,14 @@ size_t ID3_Tag::PaddingSize(size_t curSize) const
   // if the old tag was large enough to hold the new tag, then we will simply
   // pad out the difference - that way the new tag can be written without
   // shuffling the rest of the song file around
-  if (__starting_bytes && (__starting_bytes >= curSize) && 
-      (__starting_bytes - curSize) < ID3_PADMAX)
+  if (this->GetPrependedBytes() && (this->GetPrependedBytes() >= curSize) && 
+      (this->GetPrependedBytes() - curSize) < ID3_PADMAX)
   {
-    newSize = __starting_bytes;
+    newSize = this->GetPrependedBytes();
   }
   else
   {
-    luint tempSize = curSize + __file_size;
+    luint tempSize = curSize + ID3_GetDataSize(*this);
     
     // this method of automatic padding rounds the COMPLETE FILE up to the
     // nearest 2K.  If the file will already be an even multiple of 2K (with
@@ -557,7 +349,7 @@ size_t ID3_Tag::PaddingSize(size_t curSize) const
     tempSize = ((tempSize / ID3_PADMULTIPLE) + 1) * ID3_PADMULTIPLE;
     
     // the size of the new tag is the new filesize minus the audio data
-    newSize = tempSize - __file_size;
+    newSize = tempSize - ID3_GetDataSize(*this);
   }
   
   return newSize - curSize;
