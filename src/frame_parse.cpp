a346318cc8a020c9b91cc876c9ab9fc27058d78f
eldamitri <>
2000-09-27 08:30:08 +0000

    (Parse): Reimplemented to use new Parse(ID3_Reader&) method, will be soon deprecated

--- src/frame_parse.cpp
+++ src/frame_parse.cpp
@@ -25,60 +25,87 @@
 // http://download.sourceforge.net/id3lib/
 
 #include <zlib.h>
-#include "frame.h"
-#include "utils.h"
 
 #if defined HAVE_CONFIG_H
 #include <config.h>
 #endif
 
+#include "debug.h"
+
+#include "frame.h"
+#include "utils.h"
+#include "reader_decorators.h"
+#include "readers.h"
+
 size_t ID3_Frame::Parse(const uchar * const buffer, size_t size) 
 { 
-  _bad_parse = false;  
-  const size_t hdr_size = _hdr.Parse(buffer, size);  
+  id3::MemoryReader mr(reinterpret_cast<const char *>(buffer), size);
+  ID3_Reader::pos_type beg = mr.getCur();
+  this->Parse(mr);
+  return mr.getCur() - beg;
+}
 
-  if (!hdr_size)  
-  {  
-    return 0;  
+void ID3_Frame::Parse(ID3_Reader& reader) 
+{ 
+  ID3D_NOTICE( "ID3_Frame::Parse(): reader.getBeg() = " << reader.getBeg() );
+  ID3D_NOTICE( "ID3_Frame::Parse(): reader.getCur() = " << reader.getCur() );
+  ID3D_NOTICE( "ID3_Frame::Parse(): reader.getEnd() = " << reader.getEnd() );
+  ID3_Reader::pos_type beg = reader.getCur();
+  _bad_parse = false;  
+  _hdr.Parse(reader);  
+  ID3D_NOTICE( "ID3_Frame::Parse(): after hdr, getCur() = " << reader.getCur() );
+
+  if (reader.getCur() == beg)  
+  { 
+    ID3D_WARNING( "ID3_Frame::Parse(): hdr size is 0" );
+    reader.setCur(beg);
+    return;  
   }  
+  ID3D_NOTICE( "ID3_Frame::Parse(): found frame! id = " << _hdr.GetTextID() );
 
   // data is the part of the frame buffer that appears after the header  
-  const uchar* data = &buffer[hdr_size]; 
   const size_t data_size = _hdr.GetDataSize();
-  size_t extras = 0;
-  // how many bytes remain to be parsed 
-  size_t remainder = data_size - MIN(extras, data_size);
+  ID3D_NOTICE( "ID3_Frame::Parse(): data_size = " << data_size );
+  id3::WindowedReader wr(reader);
+  wr.setWindow(wr.getCur(), data_size);
+  ID3D_NOTICE( "ID3_Frame::Parse(): window getBeg() = " << wr.getBeg() );
+  ID3D_NOTICE( "ID3_Frame::Parse(): window getCur() = " << wr.getCur() );
+  ID3D_NOTICE( "ID3_Frame::Parse(): window getEnd() = " << wr.getEnd() );
   
   unsigned long expanded_size = 0;
   if (_hdr.GetCompression())
   {
-    expanded_size = ParseNumber(&data[extras]);
-    extras += sizeof(uint32);
+    id3::NumberReader nr(wr);
+    expanded_size = nr.readNumber(sizeof(uint32));
+    ID3D_NOTICE( "ID3_Frame::Parse(): frame is compressed, expanded_size = " << expanded_size );
   }
 
   if (_hdr.GetEncryption())
   {
-    this->_SetEncryptionID(data[extras++]);
+    char ch = wr.readChar();
+    this->_SetEncryptionID(ch);
+    ID3D_NOTICE( "ID3_Frame::Parse(): frame is encrypted, encryption_id = " << (int) ch );
   }
 
   if (_hdr.GetGrouping())
   {
-    this->_SetGroupingID(data[extras++]);
+    char ch = wr.readChar();
+    this->_SetGroupingID(ch);
+    ID3D_NOTICE( "ID3_Frame::Parse(): frame is encrypted, grouping_id = " << (int) ch );
   }
 
-  data += extras;
+  ID3_Reader* rdr;
 
   // expand out the data if it's compressed 
-  uchar* expanded_data = NULL;
-  if (_hdr.GetCompression())
-  {  
-    expanded_data = new uchar[expanded_size];  
-        
-    uncompress(expanded_data, &expanded_size, data, remainder);  
-    data = expanded_data; 
-    remainder = expanded_size; 
+  if (!_hdr.GetCompression())
+  {
+    rdr = &wr;
   }
-  
+  else
+  {
+    rdr = new id3::CompressedStreamReader(wr, expanded_size);
+  }
+
   // set the type of frame based on the parsed header  
   this->_ClearFields(); 
   this->_InitFields(); 
@@ -86,60 +113,58 @@ size_t ID3_Frame::Parse(const uchar * const buffer, size_t size)
   ID3_TextEnc enc = ID3TE_ASCII;  // set the default encoding 
   ID3_V2Spec spec = this->GetSpec(); 
   // parse the frame's fields  
+  ID3D_NOTICE( "ID3_Frame::Parse(): num_fields = " << _num_fields );
   for (ID3_Field** fi = _fields; fi != _fields + _num_fields; fi++)
   {
+    size_t field_num = fi - _fields;
+    ID3D_NOTICE( "ID3_Frame::Parse(): field #" << field_num );
     if (!*fi)
     {
       // Ack!  Why is the field NULL?  Log this...
+      ID3D_WARNING( "ID3_Frame::Parse(): field is null" );
       continue;
     }
-    if (remainder == 0) 
+
+    if (rdr->peekChar() == ID3_Reader::END_OF_READER) 
     { 
       // there's no remaining data to parse! 
+      ID3D_WARNING( "ID3_Frame::Parse(): frame is out of data at postion " << rdr->getCur() );
       _bad_parse = true; 
       break; 
     } 
  
     if (!(*fi)->InScope(spec)) 
-    { 
-      // this field isn't in scope, so don't attempt to parse into it 
-      // rather, give it some reasonable default value in case someone tries
-      // to access it
-      switch ((*fi)->GetType())
-      {
-        case ID3FTY_INTEGER:
-          **fi = (uint32) 0;
-          break;
-        default:
-          **fi = "";
-          break;
-      }
-      // now continue with the rest of the fields
+    {
+      ID3D_NOTICE( "ID3_Frame::Parse(): field is not in scope" );
+      // continue with the rest of the fields
       continue; 
     }
       
     (*fi)->SetEncoding(enc);
-    size_t frame_size = (*fi)->Parse(data, remainder); 
+    beg = rdr->getCur();
+    ID3D_NOTICE( "ID3_Frame::Parse(): parsing field, cur = " << beg );
+    ID3D_NOTICE( "ID3_Frame::Parse(): parsing field, end = " << rdr->getEnd() );
+    (*fi)->Parse(*rdr);
       
-    if (0 == frame_size) 
+    if (rdr->getCur() == beg) 
     { 
       // nothing to parse!  ack!  parse error... 
+      ID3D_WARNING( "ID3_Frame::Parse(): no data parsed, bad parse" );
       _bad_parse = true; 
-      break; 
+      break;
     } 
  
     if ((*fi)->GetID() == ID3FN_TEXTENC)  
     {
       enc = static_cast<ID3_TextEnc>((*fi)->Get());  
-    }  
-      
-    data += frame_size; 
-    remainder -= MIN(frame_size, remainder); 
+      ID3D_NOTICE( "ID3_Frame::Parse(): found encoding = " << enc );
+    }
   }  
       
   _changed = false;
 
-  delete [] expanded_data;
- 
-  return MIN(hdr_size + data_size, size);  
+  if (rdr != &wr)
+  {
+    delete rdr;
+  }
 } 
