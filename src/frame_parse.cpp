cb1da5b7d07488df7250018b10c9739255c602c3
eldamitri <>
2000-10-01 00:31:18 +0000

    - Class ID3_Frame is now simply an interface.  It will pass off all of   its funcitonality to a pointer to ID3_FrameImpl, which is its only   data member.  In this way, the implementation of the ID3_Frame can   more easily change without affecting its interface.  ID3_Frame can't   serve as a true "interface" (that is, abstract base class) since so   much code already relies on the ability to create stack-based frame   objects, but this is pretty close. - Added methods SetEncryptionID(uchar), GetEncryptionID(),   SetGroupingID(uchar), and GetGroupingID()

--- src/frame_parse.cpp
+++ src/frame_parse.cpp
@@ -32,50 +32,42 @@
 
 #include "debug.h"
 
-#include "frame.h"
+#include "frame_impl.h"
 #include "utils.h"
 #include "reader_decorators.h"
 #include "readers.h"
 
-size_t ID3_Frame::Parse(const uchar * const buffer, size_t size) 
-{ 
-  id3::MemoryReader mr(reinterpret_cast<const char *>(buffer), size);
-  ID3_Reader::pos_type beg = mr.getCur();
-  this->Parse(mr);
-  return mr.getCur() - beg;
-}
-
 namespace id3
 {
-  bool parseFields(ID3_Reader& rdr, ID3_Frame& frame)
+  bool parseFields(ID3_Reader& rdr, ID3_FrameImpl& frame)
   {
     ExitTrigger et(rdr);
     ID3_TextEnc enc = ID3TE_ASCII;  // set the default encoding 
     ID3_V2Spec spec = frame.GetSpec(); 
     // parse the frame's fields  
-    ID3D_NOTICE( "ID3_Frame::Parse(): num_fields = " << frame.GetNumFields() );
+    ID3D_NOTICE( "ID3_FrameImpl::Parse(): num_fields = " << frame.GetNumFields() );
     for (size_t i = 0; i < frame.GetNumFields(); ++i)
     {
       if (rdr.atEnd())
       { 
         // there's no remaining data to parse! 
-        ID3D_WARNING( "ID3_Frame::Parse(): frame is out of data at postion " <<
+        ID3D_WARNING( "ID3_FrameImpl::Parse(): frame is out of data at postion " <<
                       rdr.getCur() );
         return false;
       } 
       
-      ID3D_NOTICE( "ID3_Frame::Parse(): field #" << i );
+      ID3D_NOTICE( "ID3_FrameImpl::Parse(): field #" << i );
       ID3_Field* fp = frame.GetFieldAt(i);
       if (NULL == fp)
       {
         // Ack!  Why is the field NULL?  Log this...
-        ID3D_WARNING( "ID3_Frame::Parse(): field is null" );
+        ID3D_WARNING( "ID3_FrameImpl::Parse(): field is null" );
         continue;
       }
       
       if (!fp->InScope(spec)) 
       {
-        ID3D_NOTICE( "ID3_Frame::Parse(): field is not in scope" );
+        ID3D_NOTICE( "ID3_FrameImpl::Parse(): field is not in scope" );
         // continue with the rest of the fields
         continue; 
       }
@@ -83,74 +75,74 @@ namespace id3
       fp->SetEncoding(enc);
       ID3_Reader::pos_type beg = rdr.getCur();
       et.setExitPos(beg);
-      ID3D_NOTICE( "ID3_Frame::Parse(): parsing field, cur = " << beg );
-      ID3D_NOTICE( "ID3_Frame::Parse(): parsing field, end = " << rdr.getEnd() );
+      ID3D_NOTICE( "ID3_FrameImpl::Parse(): parsing field, cur = " << beg );
+      ID3D_NOTICE( "ID3_FrameImpl::Parse(): parsing field, end = " << rdr.getEnd() );
       if (!fp->Parse(rdr) || rdr.getCur() == beg) 
       { 
         // nothing to parse!  ack!  parse error... 
-        ID3D_WARNING( "ID3_Frame::Parse(): no data parsed, bad parse" );
+        ID3D_WARNING( "ID3_FrameImpl::Parse(): no data parsed, bad parse" );
         return false;
       }
       
       if (fp->GetID() == ID3FN_TEXTENC)  
       {
         enc = static_cast<ID3_TextEnc>(fp->Get());  
-        ID3D_NOTICE( "ID3_Frame::Parse(): found encoding = " << enc );
+        ID3D_NOTICE( "ID3_FrameImpl::Parse(): found encoding = " << enc );
       }
     }
     et.setExitPos(rdr.getCur());
   }
 };
 
-bool ID3_Frame::Parse(ID3_Reader& reader) 
+bool ID3_FrameImpl::Parse(ID3_Reader& reader) 
 { 
   id3::ExitTrigger et(reader);
-  ID3D_NOTICE( "ID3_Frame::Parse(): reader.getBeg() = " << reader.getBeg() );
-  ID3D_NOTICE( "ID3_Frame::Parse(): reader.getCur() = " << reader.getCur() );
-  ID3D_NOTICE( "ID3_Frame::Parse(): reader.getEnd() = " << reader.getEnd() );
+  ID3D_NOTICE( "ID3_FrameImpl::Parse(): reader.getBeg() = " << reader.getBeg() );
+  ID3D_NOTICE( "ID3_FrameImpl::Parse(): reader.getCur() = " << reader.getCur() );
+  ID3D_NOTICE( "ID3_FrameImpl::Parse(): reader.getEnd() = " << reader.getEnd() );
   ID3_Reader::pos_type beg = reader.getCur();
 
   if (!_hdr.Parse(reader) || reader.getCur() == beg)  
   { 
-    ID3D_WARNING( "ID3_Frame::Parse(): no header to parse" );
+    ID3D_WARNING( "ID3_FrameImpl::Parse(): no header to parse" );
     return false; 
   }
-  ID3D_NOTICE( "ID3_Frame::Parse(): after hdr, getCur() = " << reader.getCur() );
-  ID3D_NOTICE( "ID3_Frame::Parse(): found frame! id = " << _hdr.GetTextID() );
+  ID3D_NOTICE( "ID3_FrameImpl::Parse(): after hdr, getCur() = " << reader.getCur() );
+  ID3D_NOTICE( "ID3_FrameImpl::Parse(): found frame! id = " << _hdr.GetTextID() );
 
   // data is the part of the frame buffer that appears after the header  
   const size_t data_size = _hdr.GetDataSize();
-  ID3D_NOTICE( "ID3_Frame::Parse(): data_size = " << data_size );
+  ID3D_NOTICE( "ID3_FrameImpl::Parse(): data_size = " << data_size );
   if (reader.getEnd() < beg + data_size)
   {
-    ID3D_WARNING( "ID3_Frame::Parse(): not enough data to parse frame" );
+    ID3D_WARNING( "ID3_FrameImpl::Parse(): not enough data to parse frame" );
     return false;
   }
   id3::WindowedReader wr(reader, data_size);
-  ID3D_NOTICE( "ID3_Frame::Parse(): window getBeg() = " << wr.getBeg() );
-  ID3D_NOTICE( "ID3_Frame::Parse(): window getCur() = " << wr.getCur() );
-  ID3D_NOTICE( "ID3_Frame::Parse(): window getEnd() = " << wr.getEnd() );
+  ID3D_NOTICE( "ID3_FrameImpl::Parse(): window getBeg() = " << wr.getBeg() );
+  ID3D_NOTICE( "ID3_FrameImpl::Parse(): window getCur() = " << wr.getCur() );
+  ID3D_NOTICE( "ID3_FrameImpl::Parse(): window getEnd() = " << wr.getEnd() );
   
   unsigned long expanded_size = 0;
   if (_hdr.GetCompression())
   {
     id3::NumberReader nr(wr);
     expanded_size = nr.readNumber(sizeof(uint32));
-    ID3D_NOTICE( "ID3_Frame::Parse(): frame is compressed, expanded_size = " << expanded_size );
+    ID3D_NOTICE( "ID3_FrameImpl::Parse(): frame is compressed, expanded_size = " << expanded_size );
   }
 
   if (_hdr.GetEncryption())
   {
     char ch = wr.readChar();
-    this->_SetEncryptionID(ch);
-    ID3D_NOTICE( "ID3_Frame::Parse(): frame is encrypted, encryption_id = " << (int) ch );
+    this->SetEncryptionID(ch);
+    ID3D_NOTICE( "ID3_FrameImpl::Parse(): frame is encrypted, encryption_id = " << (int) ch );
   }
 
   if (_hdr.GetGrouping())
   {
     char ch = wr.readChar();
-    this->_SetGroupingID(ch);
-    ID3D_NOTICE( "ID3_Frame::Parse(): frame is encrypted, grouping_id = " << (int) ch );
+    this->SetGroupingID(ch);
+    ID3D_NOTICE( "ID3_FrameImpl::Parse(): frame is encrypted, grouping_id = " << (int) ch );
   }
 
   // set the type of frame based on the parsed header  
