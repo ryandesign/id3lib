1e7452adfc567083aa8aa1698a2947d535acabd7
eldamitri <>
2000-10-09 04:27:38 +0000

    - Removed inclusion of file error.h - Added inclusion of files wirter_decorators.h and writers_compressed.h - Removed irrelevant documentation - Added function renderFields(ID3_Writer& writer, const ID3_FrameImpl&) - Replaced method Render(uchar*) with method Render(ID3_Writer&)

--- src/frame_render.cpp
+++ src/frame_render.cpp
@@ -37,147 +37,118 @@
 #include "tag.h"
 #include "utils.h"
 #include "frame_impl.h"
-#include "error.h"
+#include "writer_decorators.h"
+#include "writers_compressed.h"
 
 using namespace dami;
 
-// Ideally, Render should render the frame in the order it is parsed, starting
-// with the header, then any extra info (resulting from header flags), and then
-// the frame data, one field at a time.  This, unfortunately, isn't possible.
-// In order to render the frame header, the data size must be known.  One could
-// query the fields' sizes before rendering them, but it would be faster to
-// render the fields and add their sizes (which is calculated and returned in
-// the fields' Render function) at the same time.
-//
-// Even if we determined this extra processing to be acceptable, it isn't
-// possible to precompute the frame data size without rendering them all when
-// compression is enabled.  When compression is enabled, then the header
-// contains the size of the compressed data.  So all the fields need to
-// rendered to a buffer and compressed before the data size can even be
-// calculated in this scenario.  And the compressed data might even be
-// discarded if it is no smaller than the uncompressed data.
-//
-// So, Render should progress more-or-less as follows.  The fields should be
-// rendered to the target buffer at its desired position (after the projected
-// header and extra bytes (at most 2)).  If compression has been specified,
-// then compress it into a temporary buffer.  If the compressed size is smaller
-// than the uncompressed data, copy the temp buffer to the correct place in the
-// target buffer.  Finally, render the header and the extra info.  The
-// encryption and grouping id's shouldn't be rendered until after the
-// compression takes place, since they will be rendered to a different spots in
-// the buffer depending on whether or not the data is compressed.  This is
-// because the uncompressed data size must come after before the id's if the
-// the data is compressed, but won't be rendered if the data isn't compressed.
-//
-// One could probably implement Render more "naturally" by using more
-// dynamically allocated buffers, but that would degrade performance. Whether
-// or not this performance degradation would cause a significant impact on the
-// overall performane of typeical apps should be looked in to.
-//
-// So, in short, Render is a more complicated function than it should be, but
-// it doesn't look like there's a much better way that's any faster.
-size_t ID3_FrameImpl::Render(uchar *buffer) const
+namespace dami
 {
-  if (NULL == buffer)
+  namespace id3
   {
-    // log this
-    return 0;
-  }
+    namespace v2
+    {
+      void renderFields(ID3_Writer& writer, const ID3_FrameImpl& frame);
+    }
+  };
+};
 
+void id3::v2::renderFields(ID3_Writer& writer, const ID3_FrameImpl& frame)
+{
+  ID3_TextEnc enc = ID3TE_ASCII;
+  for (size_t i = 0; i < frame.NumFields(); ++i)
+  {
+    ID3_Field* fld = frame.GetFieldNum(i);
+    if (fld != NULL && fld->InScope(frame.GetSpec()))
+    {
+      if (fld->GetID() == ID3FN_TEXTENC)  
+      {
+        enc = static_cast<ID3_TextEnc>(fld->Get());  
+        ID3D_NOTICE( "id3::v2::renderFields(): found encoding = " << enc );
+      }
+      else
+      {
+        fld->SetEncoding(enc);
+      }
+      fld->Render(writer);
+    }
+  }
+}
+
+void ID3_FrameImpl::Render(ID3_Writer& writer) const
+{
   // Return immediately if we have no fields, which (usually) means we're
   // trying to render a frame which has been Cleared or hasn't been initialized
-  if (!_num_fields)
+  if (!this->NumFields())
   {
-    return 0;
+    return;
   }
 
-  uchar e_id = this->GetEncryptionID(), g_id = this->GetGroupingID();
-  size_t decompressed_size = 0;
-  size_t extras = ( e_id > 0 ? 1 : 0 ) + ( g_id > 0 ? 1 : 0 );
-  ID3_FrameHeader hdr = _hdr;
+  ID3_FrameHeader hdr;
   
   const size_t hdr_size = hdr.Size();
 
   // 1.  Write out the field data to the buffer, with the assumption that
   //     we won't be decompressing, since this is the usual behavior
-  ID3_TextEnc enc = ID3TE_ASCII;
-  size_t data_size = 0;
-    
-  for (ID3_Field** fi = _fields; fi != _fields + _num_fields; fi++)
+  String flds;
+  io::StringWriter fldWriter(flds);
+  size_t origSize = 0;
+  if (!this->GetCompression())
   {
-    if ((*fi)->GetID() == ID3FN_TEXTENC)  
-    {
-      enc = static_cast<ID3_TextEnc>((*fi)->Get());  
-    }
-    
-    if (*fi && (*fi)->InScope(ID3V2_LATEST))
-    {
-      (*fi)->SetEncoding(enc);
-      data_size += (*fi)->Render(&buffer[data_size + hdr_size + extras]);
-    }
+    id3::v2::renderFields(fldWriter, *this);
+    origSize = flds.size();
+    ID3D_NOTICE ( "ID3_FrameImpl::Render(): uncompressed fields" );
   }
-  
-  // Return if there's no data
-  if (!data_size)
+  else
   {
-    return 0;
+    io::CompressedWriter cr(fldWriter);
+    id3::v2::renderFields(cr, *this);
+    cr.flush();
+    origSize = cr.getOrigSize();
+    ID3D_NOTICE ( "ID3_FrameImpl::Render(): compressed fields, orig size = " <<
+                  origSize );
   }
-  
-  // 2.  Attempt to compress if specified
-  if (this->GetCompression())
+
+  size_t fldSize = flds.size();
+  ID3D_NOTICE ( "ID3_FrameImpl::Render(): field size = " << fldSize );
+  if (fldSize == 0)
   {
-    // The zlib documentation specifies that the destination size needs to be
-    // an unsigned long at least 0.1% larger than the source buffer, plus 12
-    // bytes
-    unsigned long new_data_size = data_size + (data_size / 10) + 12;
-      
-    uchar* compressed_data = new uchar[new_data_size];
-
-    if (compress(compressed_data, &new_data_size, &buffer[hdr_size + extras],
-                 data_size) != Z_OK)
-    {
-      // log this
-      delete [] compressed_data;
-      return 0;
-      //ID3_THROW(ID3E_zlibError);
-    }
-
-    // if the compression actually saves space...
-    if ((new_data_size + sizeof(uint32)) < data_size)
-    {
-      // add 4 bytes to 'extras' for the decompressed size
-      extras += sizeof(uint32);
-
-      memcpy(&buffer[hdr_size + extras], compressed_data, new_data_size);
-
-      decompressed_size = data_size;
-      data_size = new_data_size;
-    }
-    
-    delete [] compressed_data;
+    ID3D_WARNING ( "ID3_FrameImpl::Render(): no field data" );
+    return;
   }
   
   // determine which flags need to be set
-  hdr.SetDataSize(data_size + extras);
-  hdr.SetEncryption(e_id > 0);
-  hdr.SetGrouping(g_id > 0);
-  hdr.SetCompression(decompressed_size > 0);
+  uchar eID = this->GetEncryptionID(), gID = this->GetGroupingID();
+  hdr.SetFrameID(this->GetID());
+  hdr.SetEncryption(eID > 0);
+  hdr.SetGrouping(gID > 0);
+  hdr.SetCompression(origSize > fldSize);
+  hdr.SetDataSize(fldSize + ((hdr.GetCompression() ? 4 : 0) + 
+                             (hdr.GetEncryption()  ? 1 : 0) + 
+                             (hdr.GetGrouping()    ? 1 : 0)));
 
-  hdr.Render(buffer);
-  uchar* data = buffer + hdr_size;
-  if (decompressed_size)
+  // write out the header
+  hdr.Render(writer);
+
+  // No-man's land!  Not part of the header, not part of the data
+  if (hdr.GetCompression())
   {
-    data += ::renderNumber(data, decompressed_size);
+    io::BinaryNumberWriter bnr(writer);
+    bnr.writeNumber(origSize, sizeof(uint32));
+    ID3D_NOTICE( "ID3_FrameImpl::Render(): frame is compressed, wrote origSize = " << origSize );
   }
-  if (e_id)
+  if (hdr.GetEncryption())
   {
-    *data++ = e_id;
+    writer.writeChar(eID);
+    ID3D_NOTICE( "ID3_FrameImpl::Render(): frame is compressed, encryption id = " << eID );
   }
-  if (g_id)
+  if (hdr.GetGrouping())
   {
-    *data++ = g_id;
+    writer.writeChar(gID);
+    ID3D_NOTICE( "ID3_FrameImpl::Render(): frame is compressed, grouping id = " << gID );
   }
+
+  // Write the field data
+  writer.writeChars(flds.data(), fldSize);
   _changed = false;
-    
-  return hdr_size + extras + data_size;
 }
