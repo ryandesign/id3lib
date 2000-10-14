a9d5f7cb522adcba80093e6c76139a7c6bf6ea10
eldamitri <>
2000-10-14 19:03:25 +0000

    BString is "unsigned char" again

--- include/id3/strings.h
+++ include/id3/strings.h
@@ -6,9 +6,8 @@
 namespace dami
 {
   typedef std::basic_string<char>           String;
-  typedef std::basic_string<char>          BString;
+  typedef std::basic_string<unsigned char> BString;
   typedef std::basic_string<wchar_t>       WString;
-  //typedef std::basic_string<unsigned char> BString;
 };
 
 #endif /* _ID3LIB_STRINGS_H_ */
