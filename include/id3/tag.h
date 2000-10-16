8f9d6441bbc1e8ae424cb516e81ce1d8f4206579
eldamitri <>
2000-10-16 06:59:06 +0000

    class ID3_Tag no longer inherits from ID3_Speccable

--- include/id3/tag.h
+++ include/id3/tag.h
@@ -30,13 +30,12 @@
 
 #include "frame.h"
 #include "field.h"
-#include "spec.h"
 
 class ID3_Reader;
 class ID3_Writer;
 class ID3_TagImpl;
 
-class ID3_Tag : public ID3_Speccable
+class ID3_Tag
 {
   ID3_TagImpl* _impl;
 public:
