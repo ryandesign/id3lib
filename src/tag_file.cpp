865b81a2e73b37ac61f291ff138a9bd2eeaaf202
shadrack <>
2001-12-16 09:10:14 +0000

    Daniel Hazelbaker's patch to fix ID3V1 appending when only ID3V2 tag was read

--- src/tag_file.cpp
+++ src/tag_file.cpp
@@ -157,7 +157,11 @@ size_t RenderV1ToFile(ID3_TagImpl& tag, fstream& file)
     return 0;
   }
 
-  if (ID3_V1_LEN > tag.GetAppendedBytes())
+  // Heck no, this is stupid.  If we do not read in an initial V1(.1)
+  // header then we are constantly appending new V1(.1) headers. Files
+  // can get very big that way if we never overwrite the old ones.
+  //  if (ID3_V1_LEN > tag.GetAppendedBytes())   - Daniel Hazelbaker
+  if (ID3_V1_LEN > tag.GetFileSize())
   {
     file.seekp(0, ios::end);
   }
