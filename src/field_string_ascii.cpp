f59e6184fc40eb44f77b14b50a2df8853c7b8183
t1mpy <>
2002-09-13 15:38:34 +0000

    Changed UNICODE and ASCII to appropriate codepage

--- src/field_string_ascii.cpp
+++ src/field_string_ascii.cpp
@@ -2,6 +2,7 @@
 
 // id3lib: a C++ library for creating and manipulating id3v1/v2 tags
 // Copyright 1999, 2000  Scott Thomas Haug
+// Copyright 2002 Thijmen Klok (thijmen@id3lib.org)
 
 // This library is free software; you can redistribute it and/or modify it
 // under the terms of the GNU Library General Public License as published by
@@ -58,41 +59,41 @@ size_t ID3_FieldImpl::Set(const char* data)
 /** Copies the contents of the field into the supplied buffer, up to the
  ** number of characters specified; for fields with multiple entries, the
  ** optional third parameter indicates which of the fields to retrieve.
- ** 
+ **
  ** The third parameter is useful when using text lists (see Add(const char*)
  ** for more details).  The default value for this third parameter is 1,
  ** which returns the entire string if the field contains only one item.
- ** 
+ **
  ** It returns the number of characters (not bytes necessarily, and not
  ** including any NULL terminator) of the supplied buffer that are now used.
- ** 
+ **
  ** \code
  **   char myBuffer[1024];
  **   size_t charsUsed = myFrame.GetField(ID3FN_TEXT)->Get(buffer, 1024);
  ** \endcode
- ** 
+ **
  ** It fills the buffer with as much data from the field as is present in the
  ** field, or as large as the buffer, whichever is smaller.
- ** 
+ **
  ** \code
  **   char myBuffer[1024];
  **   size_t charsUsed = myFrame.GetField(ID3FN_TEXT)->Get(buffer, 1024, 3);
  ** \endcode
- ** 
+ **
  ** This fills the buffer with up to the first 1024 characters from the third
  ** element of the text list.
- ** 
+ **
  ** \sa Add(const char*)
  **/
 size_t ID3_FieldImpl::Get(char* buffer, size_t maxLength) const
 {
   size_t size = 0;
-  if (this->GetType() == ID3FTY_TEXTSTRING && 
-      this->GetEncoding() == ID3TE_ASCII &&
+  if (this->GetType() == ID3FTY_TEXTSTRING &&
+      ID3TE_IS_SINGLE_BYTE_ENC(this->GetEncoding()) &&
       buffer != NULL && maxLength > 0)
   {
     String data = this->GetText();
-    size = dami::min(maxLength, data.size());
+    size = min(maxLength, data.size());
     ::memcpy(buffer, data.data(), size);
     if (size < maxLength)
     {
@@ -107,11 +108,11 @@ size_t ID3_FieldImpl::Get(char* buf, size_t maxLen, size_t index) const
 {
   size_t size = 0;
   if (this->GetType() == ID3FTY_TEXTSTRING &&
-      this->GetEncoding() == ID3TE_ASCII &&
+      ID3TE_IS_SINGLE_BYTE_ENC(this->GetEncoding()) &&
       buf != NULL && maxLen > 0)
-  {  
+  {
     String data = this->GetTextItem(index);
-    size = dami::min(maxLen, data.size());
+    size = min(maxLen, data.size());
     ::memcpy(buf, data.data(), size);
     if (size < maxLen)
     {
@@ -135,7 +136,7 @@ String ID3_FieldImpl::GetTextItem(size_t index) const
 {
   String data;
   if (this->GetType() == ID3FTY_TEXTSTRING &&
-      this->GetEncoding() == ID3TE_ASCII)
+      ID3TE_IS_SINGLE_BYTE_ENC(this->GetEncoding()))
   {
     const char* raw = this->GetRawTextItem(index);
     if (raw != NULL)
@@ -199,15 +200,15 @@ size_t ID3_FieldImpl::SetText(String data)
 
 /** For fields which support this feature, adds a string to the list of
  ** strings currently in the field.
- ** 
+ **
  ** This is useful for using id3v2 frames such as the involved people list,
  ** composer, and part of setp.  You can use the GetNumTextItems() method to
  ** find out how many such items are in a list.
- ** 
+ **
  ** \code
  **   myFrame.GetField(ID3FN_TEXT)->Add("this is a test");
  ** \endcode
- ** 
+ **
  ** \param string The string to add to the field
  **/
 size_t ID3_FieldImpl::AddText_i(String data)
@@ -225,7 +226,7 @@ size_t ID3_FieldImpl::AddText_i(String data)
 
     // ASSERT(_fixed_size == 0)
     _text += '\0';
-    if (this->GetEncoding() == ID3TE_UNICODE)
+    if (ID3TE_IS_DOUBLE_BYTE_ENC(this->GetEncoding()))
     {
       _text += '\0';
     }
@@ -261,8 +262,8 @@ size_t ID3_FieldImpl::Add(const char* data)
 const char* ID3_FieldImpl::GetRawText() const
 {
   const char* text = NULL;
-  if (this->GetType() == ID3FTY_TEXTSTRING && 
-      this->GetEncoding() == ID3TE_ASCII)
+  if (this->GetType() == ID3FTY_TEXTSTRING &&
+      ID3TE_IS_SINGLE_BYTE_ENC(this->GetEncoding()))
   {
     text = _text.c_str();
   }
@@ -272,8 +273,8 @@ const char* ID3_FieldImpl::GetRawText() const
 const char* ID3_FieldImpl::GetRawTextItem(size_t index) const
 {
   const char* text = NULL;
-  if (this->GetType() == ID3FTY_TEXTSTRING && 
-      this->GetEncoding() == ID3TE_ASCII &&
+  if (this->GetType() == ID3FTY_TEXTSTRING &&
+      ID3TE_IS_SINGLE_BYTE_ENC(this->GetEncoding()) &&
       index < this->GetNumTextItems())
   {
     text = _text.c_str();
@@ -289,7 +290,7 @@ namespace
 {
   String readEncodedText(ID3_Reader& reader, size_t len, ID3_TextEnc enc)
   {
-    if (enc == ID3TE_ASCII)
+    if (ID3TE_IS_SINGLE_BYTE_ENC(enc))
     {
       return io::readText(reader, len);
     }
@@ -298,7 +299,7 @@ namespace
 
   String readEncodedString(ID3_Reader& reader, ID3_TextEnc enc)
   {
-    if (enc == ID3TE_ASCII)
+    if (ID3TE_IS_SINGLE_BYTE_ENC(enc))
     {
       return io::readString(reader);
     }
@@ -306,21 +307,23 @@ namespace
   }
 
   size_t writeEncodedText(ID3_Writer& writer, String data, ID3_TextEnc enc)
-  {
-    if (enc == ID3TE_ASCII)
-    {
+  { //doesn't include NULL char
+    if (ID3TE_IS_SINGLE_BYTE_ENC(enc))
       return io::writeText(writer, data);
-    }
-    return io::writeUnicodeText(writer, data);
+    else if (enc == ID3TE_UTF16BE)
+      return io::writeUnicodeText(writer, data, false);
+    else //with bom
+      return io::writeUnicodeText(writer, data, true);
   }
 
   size_t writeEncodedString(ID3_Writer& writer, String data, ID3_TextEnc enc)
-  {
-    if (enc == ID3TE_ASCII)
-    {
+  { //includes NULL char
+    if (ID3TE_IS_SINGLE_BYTE_ENC(enc))
       return io::writeString(writer, data);
-    }
-    return io::writeUnicodeString(writer, data);
+    else if (enc == ID3TE_UTF16BE)
+      return io::writeUnicodeString(writer, data, false);
+    else
+      return io::writeUnicodeString(writer, data, true);
   }
 }
 
@@ -341,18 +344,21 @@ bool ID3_FieldImpl::ParseText(ID3_Reader& reader)
     this->SetText(text);
     ID3D_NOTICE( "ID3_Field::ParseText(): fixed size string = " << text );
   }
-  else if (_flags & ID3FF_LIST)
-  {
-    ID3D_NOTICE( "ID3_Field::ParseText(): text list" );
-    // lists are always the last field in a frame.  parse all remaining 
-    // characters in the reader
-    while (!reader.atEnd())
-    {
-      String text = readEncodedString(reader, enc);
-      this->AddText(text);
-      ID3D_NOTICE( "ID3_Field::ParseText(): adding string = " << text );
-    }
-  }
+// following is commented because this is allready covered in the 'else' case
+// and *any* text (starting with identifier 'T') can contain lists, not just the defined ones
+//  else if (_flags & ID3FF_LIST)
+//  {
+//    ID3D_NOTICE( "ID3_Field::ParseText(): text list" );
+//    // lists are always the last field in a frame.  parse all remaining
+//    // characters in the reader
+//    while (!reader.atEnd())
+//    {
+//      //loop, because readEncodedString will break at null char, which is a valid char
+//      String text = readEncodedString(reader, enc);
+//      this->AddText(text);
+//      ID3D_NOTICE( "ID3_Field::ParseText(): adding string = " << text );
+//    }
+//  }
   else if (_flags & ID3FF_CSTR)
   {
     ID3D_NOTICE( "ID3_Field::ParseText(): null terminated string" );
@@ -364,11 +370,11 @@ bool ID3_FieldImpl::ParseText(ID3_Reader& reader)
   {
     ID3D_NOTICE( "ID3_Field::ParseText(): last field string" );
     String text = readEncodedText(reader, reader.remainingBytes(), enc);
-    // not null terminated.  
+    // not null terminated.
     this->AddText(text);
     ID3D_NOTICE( "ID3_Field::ParseText(): last field string = " << text );
   }
-  
+
   _changed = false;
   return true;
 }
@@ -376,7 +382,7 @@ bool ID3_FieldImpl::ParseText(ID3_Reader& reader)
 void ID3_FieldImpl::RenderText(ID3_Writer& writer) const
 {
   ID3_TextEnc enc = this->GetEncoding();
-  
+
   if (_flags & ID3FF_CSTR)
   {
     writeEncodedString(writer, _text, enc);
@@ -389,11 +395,11 @@ void ID3_FieldImpl::RenderText(ID3_Writer& writer) const
 };
 
 /** Returns the number of items in a text list.
- ** 
+ **
  ** \code
  **   size_t numItems = myFrame.GetField(ID3FN_UNICODE)->GetNumItems();
  ** \endcode
- ** 
+ **
  ** \return The number of items in a text list.
  **/
 size_t ID3_FieldImpl::GetNumTextItems() const
--- src/field_string_unicode.cpp
+++ src/field_string_unicode.cpp
@@ -2,6 +2,7 @@
 
 // id3lib: a C++ library for creating and manipulating id3v1/v2 tags
 // Copyright 1999, 2000  Scott Thomas Haug
+// Copyright 2002 Thijmen Klok (thijmen@id3lib.org)
 
 // This library is free software; you can redistribute it and/or modify it
 // under the terms of the GNU Library General Public License as published by
@@ -39,18 +40,18 @@ using namespace dami;
  **/
 
 /** \brief Copies the supplied unicode string to the field.
- ** 
+ **
  ** Performs similarly as the ASCII Set() method, taking a unicode_t string
  ** as a parameter rather than an ascii string.
- ** 
+ **
  ** \param string The unicode string to set this field to.
  ** \sa Add(const unicode_t*)
  **/
 size_t ID3_FieldImpl::Set(const unicode_t* data)
 {
   size_t size = 0;
-  if (this->GetType() == ID3FTY_TEXTSTRING && 
-      this->GetEncoding() == ID3TE_UNICODE)
+  if (this->GetType() == ID3FTY_TEXTSTRING &&
+      ID3TE_IS_DOUBLE_BYTE_ENC(this->GetEncoding()))
   {
     String text((const char*) data, ucslen(data) * 2);
     size = this->SetText_i(text);
@@ -61,8 +62,8 @@ size_t ID3_FieldImpl::Set(const unicode_t* data)
 size_t ID3_FieldImpl::Add(const unicode_t* data)
 {
   size_t size = 0;
-  if (this->GetType() == ID3FTY_TEXTSTRING && 
-      this->GetEncoding() == ID3TE_UNICODE)
+  if (this->GetType() == ID3FTY_TEXTSTRING &&
+      ID3TE_IS_DOUBLE_BYTE_ENC(this->GetEncoding()))
   {
     String text((const char*) data, ucslen(data) * 2);
     size = this->AddText_i(text);
@@ -73,17 +74,17 @@ size_t ID3_FieldImpl::Add(const unicode_t* data)
 /** Copies the contents of the field into the supplied buffer, up to the
  ** number of characters specified; for fields with multiple entries, the
  ** optional third parameter indicates which of the fields to retrieve.
- ** 
+ **
  ** Performs similarly as the ASCII Get(char *, size_t, size_t) method, taking
  ** a unicode_t string as a parameter rather than an ascii string.  The
  ** maxChars parameter still represents the maximum number of characters, not
  ** bytes.
- **   
+ **
  ** \code
  **   unicode_t myBuffer[1024];
  **   size_t charsUsed = myFrame.GetField(ID3FN_UNICODE)->Get(buffer, 1024);
- ** \endcode 
- **   
+ ** \endcode
+ **
  ** \param buffer   Where the field's data is copied to
  ** \param maxChars The maximum number of characters to copy to the buffer.
  ** \param itemNum  For fields with multiple items (such as the involved
@@ -93,12 +94,12 @@ size_t ID3_FieldImpl::Add(const unicode_t* data)
 size_t ID3_FieldImpl::Get(unicode_t *buffer, size_t maxLength) const
 {
   size_t length = 0;
-  if (this->GetType() == ID3FTY_TEXTSTRING && 
-      this->GetEncoding() == ID3TE_UNICODE &&
-      buffer != NULL && maxLength > 0)
+  if (this->GetType() == ID3FTY_TEXTSTRING &&
+    ID3TE_IS_DOUBLE_BYTE_ENC(this->GetEncoding()) &&
+    buffer != NULL && maxLength > 0)
   {
     size_t size = this->Size();
-    length = dami::min(maxLength, size);
+    length = min(maxLength, size);
     ::memcpy((void *)buffer, (void *)_text.data(), length * 2);
     if (length < maxLength)
     {
@@ -111,8 +112,8 @@ size_t ID3_FieldImpl::Get(unicode_t *buffer, size_t maxLength) const
 const unicode_t* ID3_FieldImpl::GetRawUnicodeText() const
 {
   const unicode_t* text = NULL;
-  if (this->GetType() == ID3FTY_TEXTSTRING && 
-      this->GetEncoding() == ID3TE_UNICODE)
+  if (this->GetType() == ID3FTY_TEXTSTRING &&
+      ID3TE_IS_DOUBLE_BYTE_ENC(this->GetEncoding()))
   {
     text = (unicode_t *)_text.data();
   }
@@ -122,8 +123,8 @@ const unicode_t* ID3_FieldImpl::GetRawUnicodeText() const
 const unicode_t* ID3_FieldImpl::GetRawUnicodeTextItem(size_t index) const
 {
   const unicode_t* text = NULL;
-  if (this->GetType() == ID3FTY_TEXTSTRING && 
-      this->GetEncoding() == ID3TE_UNICODE &&
+  if (this->GetType() == ID3FTY_TEXTSTRING &&
+      ID3TE_IS_DOUBLE_BYTE_ENC(this->GetEncoding()) &&
       index < this->GetNumTextItems())
   {
     String unicode = _text + '\0' + '\0';
@@ -140,14 +141,14 @@ size_t ID3_FieldImpl::Get(unicode_t *buffer, size_t maxLength, size_t itemNum) c
 {
   size_t length = 0;
   size_t total_items = this->GetNumTextItems();
-  if (this->GetType() == ID3FTY_TEXTSTRING && 
-      this->GetEncoding() == ID3TE_UNICODE &&
+  if (this->GetType() == ID3FTY_TEXTSTRING &&
+      ID3TE_IS_DOUBLE_BYTE_ENC(this->GetEncoding()) &&
       buffer != NULL && maxLength > 0 && itemNum < total_items)
   {
     const unicode_t* text = this->GetRawUnicodeTextItem(itemNum);
     if (NULL != text)
     {
-      size_t length = dami::min(maxLength, ucslen(text));
+      size_t length = min(maxLength, ucslen(text));
       ::memcpy(buffer, text, length * 2);
       if (length < maxLength)
       {
@@ -158,5 +159,6 @@ size_t ID3_FieldImpl::Get(unicode_t *buffer, size_t maxLength, size_t itemNum) c
 
   return length;
 }
+
 
 
